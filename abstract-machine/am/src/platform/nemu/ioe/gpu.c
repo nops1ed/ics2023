f56c0060e243850174f4695db48e95a0d0a191d1
tracer-ics2023 <tracer@njuics.org>
2023-10-29 15:11:25 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:11:25 up 19 min,  1 user,  load average: 0.12, 0.19, 0.13

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -38,6 +38,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     outl(SYNC_ADDR, 1);
   }
 */
+/*
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
   if (w == 0 || h == 0) return;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
@@ -48,6 +49,24 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     }
   }
   outl(SYNC_ADDR, 1);
+*/
+int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0))
+    return;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y + h; i++)
+  {
+    for (int j = x; j < x + w; j++)
+    {
+      fb[screen_w * i + j] = pixels[w * (i - y) + (j - x)];
+    }
+  }
+  if (ctl->sync)
+  {
+    outl(SYNC_ADDR, 1);
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
