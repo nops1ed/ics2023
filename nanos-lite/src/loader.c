1886a30f59101a09e3250f7ca328598540697559
tracer-ics2023 <tracer@njuics.org>
2023-10-31 16:34:49 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:34:49 up  2:44,  1 user,  load average: 0.17, 0.21, 0.20

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -53,7 +53,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
 
   // check valid elf
-  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+  assert((*(uint64_t *)ehdr.e_ident == 0x010102464c457f));
 
   Elf_Phdr phdr[ehdr.e_phnum];
   ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr) * ehdr.e_phnum);
