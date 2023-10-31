1da664ec9e4ce4c172cb4998def1ceff23b8c9d3
tracer-ics2023 <tracer@njuics.org>
2023-10-31 15:26:59 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:26:59 up  1:36,  1 user,  load average: 0.81, 0.41, 0.28

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -49,6 +49,7 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
   } Elf64_Phdr;
 */
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  /*
   Elf_Ehdr _ehdr;
   ramdisk_read(&_ehdr, 0, sizeof(_ehdr));
 
@@ -72,6 +73,25 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     ramdisk_read((void *)_p_vaddr, _poff, _p_filesz);
     memset((void *)(_p_vaddr + _p_filesz), 0, _p_memsz - _p_filesz);
   }
+  */
+
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  // check valid elf
+  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr) * ehdr.e_phnum);
+  for (int i = 0; i < ehdr.e_phnum; i++)
+  {
+    if (phdr[i].p_type == PT_LOAD)
+    {
+      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      // set .bss with zeros
+      memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    }
+  }
+  return ehdr.e_entry;
 
   return 0;
 }
