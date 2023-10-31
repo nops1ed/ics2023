2bb022bc9eebea0f6738acf514394e6e876ca1ae
tracer-ics2023 <tracer@njuics.org>
2023-10-31 21:49:32 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:49:32 up  7:42,  1 user,  load average: 0.31, 0.31, 0.27

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,9 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_SYSCALL:
+      printf("F**k you \n");
+      break;
     case EVENT_YIELD:
       printf("Huh,seems like you trap here successfully\n");
       break;
