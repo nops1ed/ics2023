59b0f3238d60cf762daab0844e599e8a6eafa51a
tracer-ics2023 <tracer@njuics.org>
2023-10-31 16:30:58 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:30:58 up  2:40,  1 user,  load average: 0.03, 0.17, 0.20

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -49,46 +49,23 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
   } Elf64_Phdr;
 */
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr _ehdr;
-  ramdisk_read(&_ehdr, 0, sizeof(Elf_Ehdr));
-
-  Elf_Phdr _phdr;
-  uint32_t _phnum = _ehdr.e_phnum;
-  uint32_t _phoff = _ehdr.e_phoff;
-  printf("The phoff = %d\n", _phoff);
-
-  //printf("f**k you\n");
-  for(int i = 0; i < _phnum; i++) {
-    printf("The offset = %d\n", _phoff + i * sizeof(_phdr));
-    ramdisk_read(&_phdr, _phoff + i * sizeof(_phdr), sizeof(_phdr));
-    if(_phdr.p_type == PT_LOAD) {
-      ramdisk_read((void *)_phdr.p_vaddr, _phdr.p_offset, _phdr.p_memsz);
-      memset((void *)(_phdr.p_vaddr + _phdr.p_filesz), 0, _phdr.p_memsz - _phdr.p_filesz);
-    }
-  }
-
-      printf("f**k you\n");
-/*
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+
   // check valid elf
   assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
 
   Elf_Phdr phdr[ehdr.e_phnum];
   ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr) * ehdr.e_phnum);
-  for (int i = 0; i < ehdr.e_phnum; i++)
-  {
-    if (phdr[i].p_type == PT_LOAD)
-    {
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    if (phdr[i].p_type == PT_LOAD) {
       ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
       // set .bss with zeros
       memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
-  return ehdr.e_entry;
-  */
 
-  return _ehdr.e_entry;
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
