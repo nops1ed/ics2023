54a9ad41f86bc47be24fef3e25defdf8995db052
tracer-ics2023 <tracer@njuics.org>
2023-11-01 23:35:58 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:35:58 up 14:09,  1 user,  load average: 0.27, 0.31, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,22 +1,18 @@
 #include <common.h>
 #include "syscall.h"
+
+int sys_yield() {
+  yield();
+  return 0;  
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   printf("syscall: a[0] has val %d\n", a[0]);
   switch (a[0]) {
-    //case SYS_yield: SYS_yield(); break;
+    case 0: sys_yield(); break;
     //case SYS_exit:  SYS_exit();  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
-
-/*
-void SYS_yield() {
-  
-}
-
-void SYS_exit() {
-  //halt();
-}
-*/
