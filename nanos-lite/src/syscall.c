20a2642ea5acb7db4de09c1d3d951a764def743f
tracer-ics2023 <tracer@njuics.org>
2023-11-02 00:01:51 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:01:51 up 14:34,  1 user,  load average: 0.45, 0.39, 0.36

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,20 +1,28 @@
 #include <common.h>
 #include "syscall.h"
 
-/*
 int sys_yield() {
   yield();
   return 0;  
 }
-*/
+
+void sys_exit() {
+  panic("No exit function defined here.");
+}
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   printf("syscall: a[0] has val %d\n", a[0]);
+  printf("syscall: a[1] has val %d\n", a[1]);
+  printf("syscall: a[2] has val %d\n", a[2]);
+  printf("syscall: a[3] has val %d\n", a[3]);
   switch (a[0]) {
-    //case 0: sys_yield(); break;
-    //case SYS_exit:  SYS_exit();  break;
+    case SYS_yield: sys_yield(); break;
+    case SYS_exit:  sys_exit();  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
