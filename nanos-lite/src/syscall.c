e52fb8fe0ecc0cd16f36350cf3661f6d3cc388a4
tracer-ics2023 <tracer@njuics.org>
2023-11-01 23:31:51 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:31:51 up 14:04,  1 user,  load average: 0.47, 0.21, 0.23

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,7 +3,7 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  printf("syscall: a[0] has val %d\n", a[0]);
   switch (a[0]) {
     //case SYS_yield: SYS_yield(); break;
     //case SYS_exit:  SYS_exit();  break;
