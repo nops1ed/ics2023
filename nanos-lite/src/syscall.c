b55da276d8c30095ac9f8f09aede154479164b57
tracer-ics2023 <tracer@njuics.org>
2023-11-02 13:22:57 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:22:57 up 1 day,  3:55,  1 user,  load average: 0.03, 0.18, 0.30

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -17,7 +17,7 @@ static void sys_exit(Context *c) {
 #ifdef CONFIG_STRACE
   printf("sys_exit(%d) = 0\n", c->GPR2);
 #endif
-  halt(c->GPR2);
+  halt((int)c->GPR2);
 }
 
 static void sys_write(Context *c) {
@@ -56,18 +56,17 @@ static void sys_getpid(Context *c) {
   panic("Not implement");
 }
 
-
 void (*syscall_table[])() = {
   &sys_exit, &sys_yield, &sys_open, &sys_read, &sys_write, &sys_kill,
   &sys_getpid, &sys_close, &sys_lseek, &sys_brk,
 };
 
-#define NR_SYST sizeof(syscall_table) / sizeof(syscall_table[0])
+#define NR_SYST (sizeof(syscall_table) / sizeof(syscall_table[0]))
 
 void do_syscall(Context *c) {
   uintptr_t a = c->GPR1;
-    if(a >= 0 && a < NR_SYST)
-      syscall_table[a](c);
-    else 
-      panic("Unhandled syscall ID = %d", a);
+  if(a >= 0 && a < NR_SYST)
+    syscall_table[a](c);
+  else 
+    panic("Unhandled syscall ID = %d", a);
 }
