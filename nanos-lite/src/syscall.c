065ca78572c373708cf2fcd6f495d33480ea5c05
tracer-ics2023 <tracer@njuics.org>
2023-11-04 15:00:27 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:00:27 up  1:03,  1 user,  load average: 0.19, 0.40, 0.41

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -131,12 +131,11 @@ static void sys_times(Context *c) {
 }
 
 static void sys_gettimeofday(Context *c) {
-  //timeval *time = (timeval *)c->GPR2;
-  static AM_TIMER_UPTIME_T timestamp;
+  static AM_TIMER_UPTIME_T time;
   int ret_val = 0;
-  ioe_read(AM_TIMER_UPTIME, &timestamp);
-  ((timeval *)c->GPR2)->tv_usec = (int32_t)timestamp.us;
-  ((timeval *)c->GPR2)->tv_sec = (int32_t)timestamp.us / 1000000;
+  ioe_read(AM_TIMER_UPTIME, &time);
+  ((timeval *)c->GPR2)->tv_usec = time.us;
+  ((timeval *)c->GPR2)->tv_sec = time.us / 1000000;
   c->GPRx = ret_val;
 #ifdef CONFIG_STRACE
   fs_curfilename();
