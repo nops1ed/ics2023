288cd3a730b3a478f4e9d80a0ac173e78622e873
tracer-ics2023 <tracer@njuics.org>
2023-11-04 14:27:48 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:27:48 up 31 min,  1 user,  load average: 0.20, 0.30, 0.29

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -5,6 +5,7 @@
 int main(void) {
     struct timeval *tv;
     int half_sec = 500000;
+    printf("Entering program...\n");
     while(1) {
         gettimeofday(tv, NULL);
         while(tv->tv_usec < half_sec) {
