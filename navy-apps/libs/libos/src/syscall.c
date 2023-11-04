aa75ecce1653d1bbd5dded4fa5f1334b53856771
tracer-ics2023 <tracer@njuics.org>
2023-11-04 14:49:51 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:49:51 up 53 min,  1 user,  load average: 0.57, 0.36, 0.35

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -104,7 +104,7 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
   *  };
   *  and gives the number of seconds and microseconds since the Epoch (see time(2)). 
   */
-  return _syscall_((intptr_t)SYS_gettimeofday, (intptr_t)tv->tv_usec, (intptr_t)tv->tv_sec, (intptr_t)tz);
+  return _syscall_((intptr_t)SYS_gettimeofday, (intptr_t)&(tv->tv_usec), (intptr_t)&(tv->tv_sec), (intptr_t)tz);
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
