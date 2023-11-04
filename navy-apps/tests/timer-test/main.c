68794f21326cacc5ca1fd2ec73fccb40cd34d4ca
tracer-ics2023 <tracer@njuics.org>
2023-11-04 14:37:11 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:37:11 up 40 min,  1 user,  load average: 0.39, 0.47, 0.40

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -9,6 +9,7 @@ int main(void) {
     for(int i = 1; i < 1000; i++) {
         do {
             gettimeofday(&tv, NULL);
+            printf("now usec have val %ld\n", tv.tv_usec);
         } while(tv.tv_usec < half_sec * i);
         printf("hello from timer %d time\n", i);
     }
