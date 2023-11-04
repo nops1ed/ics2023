781e1e612ca2dfdf3729da6bd029af179d390d61
tracer-ics2023 <tracer@njuics.org>
2023-11-04 17:11:24 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:11:24 up  3:14,  1 user,  load average: 0.52, 0.32, 0.29

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -26,10 +26,9 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-
-  assert(evtdev);
+  buf[0] = '\0';
   int ret_val = read(evtdev, buf, len);
-  return (ret_val == 0);
+  return ret_val;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
