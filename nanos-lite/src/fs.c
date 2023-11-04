7a1fddce0e185f824e1826748123d1f7ed09e93b
tracer-ics2023 <tracer@njuics.org>
2023-11-04 22:59:06 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:59:06 up  3:28,  1 user,  load average: 0.81, 0.49, 0.38

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,6 @@
 #include <fs.h>
 
+size_t fb_write(const void *buf, size_t offset, size_t len);
 size_t dispinfo_read(void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len); 
 size_t serial_write(const void *buf, size_t offset, size_t len);
@@ -34,7 +35,7 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
-  [FD_FB]     = {"/dev/fb", 0, 0, 0, invalid_read, invalid_write},
+  [FD_FB]     = {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
                 {"/dev/events", 0, 0, 0, events_read, invalid_write},
                 {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
