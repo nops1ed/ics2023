949077c63ba79d46d34794d14ed464dc94f17f21
tracer-ics2023 <tracer@njuics.org>
2023-11-05 10:17:04 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:17:04 up 14:46,  1 user,  load average: 0.41, 0.47, 0.41

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -60,7 +60,7 @@ void init_fs() {
   }
   AM_GPU_CONFIG_T gpuinfo;
   ioe_read(AM_GPU_CONFIG, &gpuinfo);
-  file_table[FD_FB].size = gpuinfo.width * gpuinfo.height;
+  file_table[FD_FB].size = gpuinfo.width * gpuinfo.height * 4;
   printf("sFS: FD_FB was initialized as %d\n", file_table[FD_FB].size);
 }
 
