e3e9901d3018c648f98205bd98013894c994b90c
tracer-ics2023 <tracer@njuics.org>
2023-11-07 22:57:51 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:57:51 up  6:58,  1 user,  load average: 0.20, 0.27, 0.25

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -30,12 +30,12 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     if(unit == 1)
       /* Copy pixel stream. */ 
       for(uint32_t col = 0; col < w; col++)
-        _buf[row * w + col] = s -> pixels[(row + x) * (s -> w) * unit + y + col];
+        _buf[row * w + col] = s -> pixels[(row + y) * (s -> w) * unit + x + col];
     /* Color depth is 32. */
     else
       for(uint32_t col = 0; col < w; col++)
         for(int i = 0; i < 4; i++)
-          _buf[row * w + col * 4 + i] = s -> pixels[(row + x) * (s -> w) + y + col * 4 + i];
+          _buf[row * w + col * 4 + i] = s -> pixels[(row + y) * (s -> w) + x + col * 4 + i];
     printf("SDL: BUF initialized successfullly\n");
     NDL_DrawRect((uint32_t *)_buf, x, y, w, h);
 
