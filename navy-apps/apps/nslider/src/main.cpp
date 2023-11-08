bc8f87638e889c0ce5c475efd0268f4bf94b18f9
tracer-ics2023 <tracer@njuics.org>
2023-11-09 05:38:34 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:38:34 up 20:30,  1 user,  load average: 0.32, 0.39, 0.35

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -20,15 +20,11 @@ static SDL_Surface *slide = NULL;
 static int cur = 0;
 
 void render() {
-  //printf("Freeing...\n");
   if (slide) {
     SDL_FreeSurface(slide);
   }
-  //printf("Free success !\n");
-  //printf("Loading...\n");
   char fname[256];
   sprintf(fname, path, cur);
-  //printf("Load success ~\n");
   slide = SDL_LoadBMP(fname);
   assert(slide);
   SDL_UpdateRect(slide, 0, 0, 0, 0);
@@ -36,23 +32,17 @@ void render() {
 
 void prev(int rep) {
   if (rep == 0) rep = 1;
-//printf("rep get ~\n");
   //printf("Now cur is %d rep is %d\n", cur, rep);
   cur -= rep;
- //printf("cur get ~\n"); 
   if (cur < 0) cur = 0;
   render();
 }
 
 void next(int rep) {
-  //printf("In next now...\n");
   if (rep == 0) rep = 1;
-  //printf("rep get ~\n");
-  //printf("Now cur is %d rep is %d\n", cur, rep);
+  printf("Now cur is %d rep is %d\n", cur, rep);
   cur += rep;
-  //printf("Cur get ~\n");
   if (cur >= N) cur = N - 1;
-  //printf("Calling render...\n");
   render();
 }
 
@@ -66,9 +56,7 @@ int main() {
 
   while (1) {
     SDL_Event e;
-    //printf("\n");
     SDL_WaitEvent(&e);
-    //printf("Get...\n");
 
     if (e.type == SDL_KEYDOWN) {
       switch(e.key.keysym.sym) {
