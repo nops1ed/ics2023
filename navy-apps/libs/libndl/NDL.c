5c0e8acd846200c471f781096ca2db87a078109e
tracer-ics2023 <tracer@njuics.org>
2023-11-09 00:29:22 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:29:22 up 15:21,  1 user,  load average: 0.56, 0.35, 0.28

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -70,7 +70,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     //printf("NDL: writing to %ld\n", x + (y + row) * disp_w);
     write(fbdev, pixels + row * w, w);  
   }
-  write(fbdev, 0, 0);
+  //write(fbdev, 0, 0);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
