ba9de579bf0771b2c2a7d820a466405e0d2cdf5c
tracer-ics2023 <tracer@njuics.org>
2023-11-09 05:31:50 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:31:50 up 20:23,  1 user,  load average: 0.33, 0.25, 0.26

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -83,7 +83,7 @@ int main() {
         case SDLK_8: rep = rep * 10 + 8; break;
         case SDLK_9: rep = rep * 10 + 9; break;
         case SDLK_J:
-        case SDLK_DOWN: next(rep); rep = 0; g = 0; break;
+        case SDLK_DOWN: printf("event J detected ~\n"); next(rep); rep = 0; g = 0; break;
         case SDLK_K:
         case SDLK_UP: printf("event K detected ~\n");prev(rep); rep = 0; g = 0; break;
         case SDLK_G:
