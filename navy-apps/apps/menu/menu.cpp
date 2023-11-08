54eb814effa9cfec042328b5c034476b8eb98c8d
tracer-ics2023 <tracer@njuics.org>
2023-11-08 23:35:02 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:35:02 up 14:26,  1 user,  load average: 0.23, 0.27, 0.26

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -65,6 +65,7 @@ static void prev() {
 
 static void clear_display(void) {
   SDL_FillRect(screen, NULL, 0xffffff);
+  printf("Call back ~\n");
 }
 
 int main(int argc, char *argv[], char *envp[]) {
