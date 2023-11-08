8ac44d3b30edf9d8bf099a1cd48dec257c8fa761
tracer-ics2023 <tracer@njuics.org>
2023-11-08 23:37:14 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:37:14 up 14:29,  1 user,  load average: 0.41, 0.30, 0.27

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -145,6 +145,7 @@ static void display_menu(int n) {
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   printf("Ready ot trap into blit ~\n");
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
+  printf("Blit Call back ~\n");
   printf("Available applications:\n");
   char buf[80];
   int i;
