44c4743244183cd3eba946cb3a55c7e1b13e6c21
tracer-ics2023 <tracer@njuics.org>
2023-11-09 05:39:28 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:39:28 up 20:31,  1 user,  load average: 0.65, 0.47, 0.38

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -40,7 +40,7 @@ void prev(int rep) {
 
 void next(int rep) {
   if (rep == 0) rep = 1;
-  //printf("Now cur is %d rep is %d\n", cur, rep);
+  printf("Now cur is %d rep is %d\n", cur, rep);
   cur += rep;
   if (cur >= N) cur = N - 1;
   render();
@@ -71,9 +71,9 @@ int main() {
         case SDLK_8: rep = rep * 10 + 8; break;
         case SDLK_9: rep = rep * 10 + 9; break;
         case SDLK_J:
-        case SDLK_DOWN: printf("event J detected ~\n"); next(rep); rep = 0; g = 0; break;
+        case SDLK_DOWN: next(rep); rep = 0; g = 0; break;
         case SDLK_K:
-        case SDLK_UP: printf("event K detected ~\n");prev(rep); rep = 0; g = 0; break;
+        case SDLK_UP: prev(rep); rep = 0; g = 0; break;
         case SDLK_G:
           g ++;
           if (g > 1) {
