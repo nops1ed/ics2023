e7601f42920d24659c4fc8d8f0193487e9163fa1
tracer-ics2023 <tracer@njuics.org>
2023-11-09 05:57:33 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:57:33 up 20:49,  1 user,  load average: 0.67, 0.47, 0.44

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -15,6 +15,29 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
+  printf("In SDL_Pollevent now\n");
+  char buf[BUFLEN];
+  memset(buf, 0, BUFLEN);
+  /* Listening for events. */
+  if(!NDL_PollEvent(buf, BUFLEN)) return 0;
+  if(!strncmp(buf, "ku", 2)) {
+    event -> type = SDL_KEYUP;
+  }
+  else if(!strncmp(buf, "kd", 2)) {
+    event -> type = SDL_KEYDOWN;
+  }
+  else 
+    event -> type = SDL_USEREVENT;
+  for(int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); i++) {
+    //printf("SDL: Comparing event %s\n", keyname[i]);
+    //printf("SDL: receiving event %s\n", buf + 3);
+    if(!strncmp(buf + 3, keyname[i], strlen(keyname[i]))) {
+      event -> key.keysym.sym = i;
+      //printf("SDL: Event detected~ \n");
+      break;
+    }
+  }
+  printf("And now the key is %d\n", event -> key.keysym.sym);
   return 0;
 }
 
