5d7fd8ff667c98bb5ed5b2ee1f544e7dc7ddb980
tracer-ics2023 <tracer@njuics.org>
2023-11-10 20:52:00 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:52:00 up 52 min,  1 user,  load average: 0.66, 0.48, 0.29

--- navy-apps/libs/libSDL_image/src/SDL_stbimage.h
+++ navy-apps/libs/libSDL_image/src/SDL_stbimage.h
@@ -208,7 +208,7 @@ SDL_STBIMG_DEF SDL_Surface* STBIMG_LoadFromMemory(const unsigned char* buffer, i
 		SDL_SetError("STBIMG_LoadFromMemory(): Couldn't get image info: %s!\n", stbi_failure_reason());
 		return NULL;
 	}
-	printf("trap here success\n");
+
 	// no alpha => use RGB, else use RGBA
 	origin_has_alpha = !(img.format == STBI_grey || img.format == STBI_rgb);
 	bppToUse = STBI_rgb_alpha;
@@ -221,6 +221,7 @@ SDL_STBIMG_DEF SDL_Surface* STBIMG_LoadFromMemory(const unsigned char* buffer, i
 	}
 	img.format = bppToUse;
 
+	printf("trap here success\n");
 	ret = STBIMG__CreateSurfaceImpl(img, origin_has_alpha, 1);
 
 	if(ret == NULL)
