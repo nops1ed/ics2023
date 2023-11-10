2cad4e9073971605afd6f7f67394c62f24d167b1
tracer-ics2023 <tracer@njuics.org>
2023-11-10 16:32:29 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:32:29 up 9 min,  1 user,  load average: 0.26, 0.11, 0.04

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -13,6 +13,7 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
+  /*
   FILE *fp = fopen(filename, "R");
   if(fp == NULL) {
     printf("ERROR: Couldn't open %s\n", filename);
@@ -30,6 +31,22 @@ SDL_Surface* IMG_Load(const char *filename) {
   fclose(fp);
   free(buf); 
   return ret_surf;
+  */
+
+   FILE *img = fopen(filename, "r");
+  fseek(img, 0, SEEK_END);
+  size_t size = ftell(img);
+
+  char *img_data = malloc(sizeof (char) * size);
+  fseek(img, 0, SEEK_SET);
+  fread(img_data, size, 1, img);
+
+  SDL_Surface *img_surface = STBIMG_LoadFromMemory(img_data, size);
+
+  free(img_data);
+  
+  fclose(img);
+  return img_surface;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
