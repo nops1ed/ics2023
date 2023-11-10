22fb5cd196e1d7fddf29f91f4aeb8e48ae001edb
tracer-ics2023 <tracer@njuics.org>
2023-11-10 18:26:26 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:26:26 up  1:54,  1 user,  load average: 0.41, 0.23, 0.21

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -93,8 +93,10 @@ void fs_curfilename(void) {
 
 size_t fs_read(int fd, void *buf, size_t len) {
   //do_read(fd, buf, len);
+  printf("Now offset is %d and len is %d\n", file_table[fd].disk_offset + file_table[fd].open_offset, len);
   size_t ret_val = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   file_table[fd].open_offset += ret_val;
+  printf("Now offset is %d\n\n", file_table[fd].disk_offset + file_table[fd].open_offset);
   return ret_val;
 }
 
