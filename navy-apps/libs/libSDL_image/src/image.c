f7e8a424353cf8c2c8ce78ba197ab0508bcf45cd
tracer-ics2023 <tracer@njuics.org>
2023-11-10 16:33:56 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:33:56 up 10 min,  1 user,  load average: 0.23, 0.12, 0.04

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -13,7 +13,6 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  /*
   FILE *fp = fopen(filename, "R");
   if(fp == NULL) {
     printf("ERROR: Couldn't open %s\n", filename);
@@ -21,7 +20,7 @@ SDL_Surface* IMG_Load(const char *filename) {
   }
   fseek(fp, 0, SEEK_END);
   size_t _size = ftell(fp);
-  char *buf = (char *)malloc(sizeof(long) * _size);
+  char *buf = (char *)malloc(sizeof(char) * _size);
   fread(buf, sizeof(char), _size, fp);
   SDL_Surface *ret_surf = STBIMG_LoadFromMemory(buf, _size);
   if(ret_surf == NULL) {
@@ -31,22 +30,6 @@ SDL_Surface* IMG_Load(const char *filename) {
   fclose(fp);
   free(buf); 
   return ret_surf;
-  */
-
-   FILE *img = fopen(filename, "r");
-  fseek(img, 0, SEEK_END);
-  size_t size = ftell(img);
-
-  char *img_data = malloc(sizeof (char) * size);
-  fseek(img, 0, SEEK_SET);
-  fread(img_data, size, 1, img);
-
-  SDL_Surface *img_surface = STBIMG_LoadFromMemory(img_data, size);
-
-  free(img_data);
-  
-  fclose(img);
-  return img_surface;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
