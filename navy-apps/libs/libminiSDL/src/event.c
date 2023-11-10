865cb33dbf889b868573dca034ffde96dc07079f
tracer-ics2023 <tracer@njuics.org>
2023-11-10 14:12:27 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:12:27 up  4:54,  1 user,  load average: 1.01, 0.76, 0.50

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -18,7 +18,7 @@ int SDL_PushEvent(SDL_Event *ev) {
 static char buf[BUFLEN];
 int SDL_PollEvent(SDL_Event *ev) {
   //char *buf = (char *)malloc(sizeof(char) * BUFLEN);
-  memset(buf, 0, BUFLEN);
+  //memset(buf, 0, BUFLEN);
   /* Listening for events. */
   if(!NDL_PollEvent(buf, BUFLEN)) {
     printf("Exiting...\n");
