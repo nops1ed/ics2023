aed8d75e0d9688f1e332b5686f78a06b26b2b0d8
tracer-ics2023 <tracer@njuics.org>
2023-11-10 19:44:37 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:44:37 up  3:12,  1 user,  load average: 0.23, 0.31, 0.35

--- navy-apps/libs/libSDL_image/src/SDL_stbimage.h
+++ navy-apps/libs/libSDL_image/src/SDL_stbimage.h
@@ -190,7 +190,7 @@ SDL_STBIMG_DEF SDL_Surface* STBIMG_LoadFromMemory(const unsigned char* buffer, i
 	int inforet = 0;
 	SDL_Surface* ret = NULL;
 	int origin_has_alpha;
-	printf("I am here\n");
+
 	if(buffer == NULL)
 	{
 		SDL_SetError("STBIMG_LoadFromMemory(): passed buffer was NULL!");
