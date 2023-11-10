65b61f7d83eda20e0f824c83f168965b8470a8c1
tracer-ics2023 <tracer@njuics.org>
2023-11-10 19:23:57 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:23:57 up  2:52,  1 user,  load average: 0.26, 0.24, 0.18

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -13,6 +13,7 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
+  /*
   printf("Loading file %s\n", filename);
   FILE *fp = fopen(filename, "r");
   if(fp == NULL) {
@@ -33,6 +34,22 @@ SDL_Surface* IMG_Load(const char *filename) {
   fclose(fp);
   free(buf); 
   return ret_surf;
+  */
+
+    FILE* fp = fopen(filename, "r");
+  
+  fseek(fp, 0L, SEEK_END);
+  size_t f_size = ftell(fp);
+  fseek(fp, 0L, SEEK_SET);
+  char *buf = (char*)malloc(f_size);
+  if(fread(buf, 1, f_size, fp) != f_size) assert("read img fail!\n");
+
+  // printf("file %s with size %d, f_size is %d\n", filename, strlen(buf), f_size);
+  SDL_Surface* img = STBIMG_LoadFromMemory(buf, f_size);
+  
+  free(buf);
+  fclose(fp);
+  return img;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
