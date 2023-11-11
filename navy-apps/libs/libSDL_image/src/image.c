b59e12d8827eeaccae455d5387235c1cff723df9
tracer-ics2023 <tracer@njuics.org>
2023-11-12 06:03:31 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  06:03:31 up 15:24,  1 user,  load average: 0.51, 0.37, 0.38

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -17,61 +17,25 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  /*
-  printf("Loading file %s\n", filename);
-  int fd = open(filename, 0, 0);
-  lseek(fd, 0, SEEK_END);
-  int size = lseek(fd, 0, SEEK_CUR);
-  char *buf = (char *)malloc(sizeof(char) * size);
-  lseek(fd, 0, SEEK_SET);
-  read(fd, buf, size);
-  printf("traping...\n");
-  SDL_Surface *ret_surf = STBIMG_LoadFromMemory(buf, size);
-  printf("STB Back...\n");
-  if(ret_surf == NULL) {
-    printf("ERROR: Couldn't load %s\n",filename);
-    exit(1);
-  }
-  close(fd);
-  free(buf); 
-  printf("Read success\n");
-  return ret_surf;
-  */
   FILE *fp = fopen(filename, "r");
   if(fp == NULL) {
-    printf("ERROR: Couldn't open %s\n", filename);
+    printf("IMG_Load ERROR: Couldn't open %s\n", filename);
     exit(1);
   }
   fseek(fp, 0, SEEK_END);
   size_t _size = ftell(fp);
   fseek(fp, 0 ,SEEK_SET);
-  char *buf = (char *)malloc( _size);
+  char *buf = (char *)malloc(sizeof(char) * _size);
   fread(buf, _size, 1, fp);
   SDL_Surface *ret_surf = STBIMG_LoadFromMemory(buf, _size);
   if(ret_surf == NULL) {
-    printf("ERROR: Couldn't load %s\n",filename);
+    printf("IMG_Load ERROR: Couldn't load %s\n",filename);
     exit(1);
   }
+
   fclose(fp);
   free(buf); 
-  printf("Read success\n");
   return ret_surf;
-  /*
-  FILE* fp = fopen(filename, "r");
-  
-  fseek(fp, 0L, SEEK_END);
-  size_t f_size = ftell(fp);
-  fseek(fp, 0L, SEEK_SET);
-  char *buf = (char*)malloc(f_size);
-  if(fread(buf, 1, f_size, fp) != f_size) assert("read img fail!\n");
-
-  // printf("file %s with size %d, f_size is %d\n", filename, strlen(buf), f_size);
-  SDL_Surface* img = STBIMG_LoadFromMemory(buf, f_size);
-  
-  free(buf);
-  fclose(fp);
-  return img;
-  */
 }
 
 int IMG_isPNG(SDL_RWops *src) {
