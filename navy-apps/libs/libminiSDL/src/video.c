1e72cbc8e8332f305b7094df554eccd7d4f98990
tracer-ics2023 <tracer@njuics.org>
2023-11-12 01:32:26 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:32:26 up 10:53,  1 user,  load average: 1.06, 0.77, 0.55

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -6,124 +6,95 @@
 #include <stdio.h>
 
 
-void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect)
-{
-  // printf("dst->format->BitsPerPixe is %d\n",dst->format->BitsPerPixel);
-  // TODO();
-
+void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-  // important:!!!uint32_t not int16_t, otherwise overflow, not learn
-  // the definition from SDL_Rect!, width * height is pretty large!!
-  uint32_t s_start_pos = 0;
-  uint32_t s_sf_row_num = src->h, s_sf_col_num = src->w,
-           s_rec_row_num = src->h, s_rec_col_num = src->w;
-  if (srcrect != NULL)
-  {
-    s_start_pos = srcrect->x + srcrect->y * src->w;
-    s_rec_row_num = srcrect->h;
-    s_rec_col_num = srcrect->w;
+  /* src is the source surface to be copied, srcrect is the rectangular area to be copied, 
+  * if NULL, the entire surface is copied. dst is the destination surface, 
+  * dstrect is the destination position, only its top-left coordinate is used, ignoring its width and height. 
+  * If NULL, the destination position is (0, 0). 
+  * The function will save the final copy area in dstrect after clipping, without modifying srcrect. 
+  */
+  uint32_t src_col = src -> w, src_row = src -> h;
+  uint32_t src_pos = 0;
+  if(srcrect != NULL) {
+    src_col = srcrect -> w;
+    src_row = srcrect -> h;
+    src_pos = srcrect -> y * src -> w + srcrect -> x;
   }
-
-  uint32_t d_start_pos = 0;
-  uint32_t d_sf_row_num = dst->h, d_sf_col_num = dst->w;
-  if (dstrect != NULL)
-  {
-    d_start_pos = dstrect->x + dstrect->y * dst->w;
+  uint32_t dst_pos = dstrect == NULL ? 0 : dstrect -> y * dst -> w + dstrect -> x;
+  //printf("Copying...\n");
+  //printf("Debug info: src_col = %d src_row = %d src_pos = %d \ndst_pos = %d dst->w = %d dst->h = %d\n",src_col,src_row,src_pos,dst_pos,dst->w,dst->h);
+  if(src -> format -> BytesPerPixel == 4)
+    for(int i = 0; i < src_row; i++)
+      memcpy((uint32_t*)(dst -> pixels) + dst_pos + i * dst -> w, 
+              (uint32_t *)(src -> pixels) + src_pos + i * src -> w, sizeof(uint32_t) * src_col);
+  else if(src -> format -> BytesPerPixel == 1) {
+    //printf("8 pixels: Should not reach here\n");
+    for(int i = 0; i < src_row; i++)
+      memcpy((uint8_t *)dst -> pixels + dst_pos + i * dst -> w, 
+              (uint8_t *)src -> pixels + src_pos + i * src -> w, sizeof(uint8_t) * src_col);
   }
-
-  if (dst->format->BitsPerPixel == 32)
-  {
-    for (int row = 0; row < s_rec_row_num; ++row)
-    {
-      memcpy((uint32_t *)dst->pixels + d_start_pos + row * d_sf_col_num, 
-        (uint32_t *)src->pixels + s_start_pos + row * s_sf_col_num, s_rec_col_num * sizeof(uint32_t));
-    }
-  }
-  else if (dst->format->BitsPerPixel == 8)
-  {
-    for (int row = 0; row < s_rec_row_num; ++row)
-    {
-      memcpy((uint8_t *)dst->pixels + d_start_pos + row * d_sf_col_num,
-       (uint8_t *)src->pixels + s_start_pos + row * s_sf_col_num, s_rec_col_num * sizeof(uint8_t));
-    }
-  }
-  else
-    printf("unsupported pixel bites %d!\n", dst->format->BitsPerPixel);
+  else printf("Pixel format cannot found");
 }
 
-void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color)
-{
-  // panic();
-  // TODO();
-
-  int32_t start_pos = 0;
-  uint32_t sf_row_num = dst->h;
-  uint32_t sf_col_num = dst->w;
-  uint32_t rec_row_num = dst->h;
-  uint32_t rec_col_num = dst->w;
-  if (dstrect != NULL)
-  {
-    start_pos = dstrect->x + dstrect->y * dst->w;
-    rec_row_num = dstrect->h;
-    rec_col_num = dstrect->w;
+void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
+  assert(dst);
+  /* Assume Little endian. */
+  uint32_t dst_col = dst -> w, dst_row = dst -> h, dst_pos = 0;
+  if(dstrect != NULL) {
+    dst_col = dstrect -> w;
+    dst_row = dstrect -> h;
+    dst_pos = dstrect -> y * dst -> w + dstrect -> x;
   }
-
-  if (dst->format->BitsPerPixel == 32)
-  {
-    for (int row = 0; row < rec_row_num; ++row)
-      memset((uint32_t *)dst->pixels + start_pos + row * sf_col_num, color,
-               rec_col_num * sizeof(uint32_t));
+  //printf("Copying...\n");
+  if(dst -> format -> BytesPerPixel == 4)
+    for(int i = 0; i < dst_row; i++)
+      memset((uint32_t *)dst -> pixels + dst_pos + dst -> w * i,
+              color, sizeof(uint32_t) * dst_row);
+  else if(dst -> format -> BytesPerPixel == 1) {
+    printf("8 pixels: Should not reach here\n");
+    for(int i = 0; i < dst_row; i++)
+     memset((uint8_t *)dst -> pixels + dst_pos + dst -> w * i,
+              color, sizeof(uint8_t) * dst_col);
   }
-  else if (dst->format->BitsPerPixel == 8)
-  {
-    for (int row = 0; row < rec_row_num; ++row)
-      memset((uint8_t *)dst->pixels + start_pos + row * sf_col_num, color, 
-                rec_col_num * sizeof(uint8_t));
-  }
-  else
-    printf("unsupported pixel bites %d!\n", dst->format->BitsPerPixel);
-  return;
+  else printf("Pixel format cannot found");
+  //printf("Copy success ~\n");
 }
 
-void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h)
-{
-  // improtant: only w == 0 && h == 0, then set the s size.
-  if (w == 0 && h == 0)
-  {
-    w = s->w;
-    h = s->h;
+void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
+  //printf("SDL: x is %d y is %d w is %d h is %d\n",x ,y , w ,h);
+  if(x == 0 && y == 0 && w == 0 && h == 0) {
+    // Update the whole screen
+    w = s -> w;
+    h = s -> h;
+    //printf("SDL: Now w is %d h is %d\n",w ,h);
   }
-
-  uint32_t len = w * h;
-  uint32_t *buf = malloc(sizeof(uint32_t) * len);
-  uint32_t start_pos = x + y * s->w;
-  uint32_t i = 0;
-  for (size_t row = 0; row < h; ++row)
-  {
-    for (size_t col = 0; col < w; ++col)
-    {
-      uint32_t offset = col + row * s->w;
-      if (s->format->BitsPerPixel == 32)
-      {
-        // important: s->pixels is 8 bit!!! r g b a r g b a...
-        //            also, NDL_DrawRect buf should be a r g b a r g b....
-        s->pixels[start_pos + offset];
-        buf[i++] = s->pixels[start_pos + 4 * offset + 3] << 24 | s->pixels[start_pos + 4 * offset + 2] << 16 | s->pixels[start_pos + 4 * offset + 1] << 8 | s->pixels[start_pos + 4 * offset];
+  uint32_t pos = 0;
+  uint32_t *_buf = (uint32_t *)malloc(sizeof(uint32_t) * w * h);
+  for(uint32_t row = 0; row < h; row++)
+    /* Color depth is 8. */
+    if(s -> format -> BytesPerPixel == 1)
+      for(uint32_t col = 0; col < w; col++) {
+      /* The concept of using a palette at 8-bit color depth. */
+        printf("8 pixels: Should not reach here\n");
+        SDL_Color sdlcolor = s -> format -> palette -> colors[((row + y) * (s -> w) + x + col)];
+        _buf[pos++] = sdlcolor.a << 24 | sdlcolor.r << 16 | sdlcolor.g << 8 | sdlcolor.b;
       }
-      else if (s->format->BitsPerPixel == 8)
-      {
-        SDL_Color rgba_color = s->format->palette->colors[s->pixels[start_pos + offset]];
-        buf[i++] = rgba_color.a << 24 | rgba_color.r << 16 | rgba_color.g << 8 | rgba_color.b;
+    /* Color depth is 32. */
+    else {
+      /* Each pixel is described as a color using a 32-bit integer in the form of 00RRGGBB. */
+      for(uint32_t col = 0; col < w; col++) {
+          //uint32_t offset = ((row + y) * (s -> w) + x + col) * 4;
+          uint32_t offset = x + y * s -> w + (col + row * s->w) * 4;
+          _buf[pos++] = s -> pixels[offset + 3] << 24 | s -> pixels[offset + 2] << 16 |
+                        s -> pixels[offset + 1] << 8 | s -> pixels[offset + 0];
       }
-      else
-        printf("unsupported pixel bites %d!\n", s->format->BitsPerPixel);
     }
-  }
-
-  NDL_DrawRect(buf, x, y, w, h);
-
-  free(buf);
+    //printf("SDL: BUF initialized successfullly\n");
+    NDL_DrawRect(_buf, x, y, w, h);
+    //printf("Drawing success ~\n");
+    free(_buf);
 }
 
 // APIs below are already implemented.
