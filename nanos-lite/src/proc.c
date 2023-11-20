959b5f41ab130e5fa91bc99469cf47d882c53adc
tracer-ics2023 <tracer@njuics.org>
2023-11-20 15:11:29 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:11:29 up  5:11,  1 user,  load average: 0.50, 0.70, 0.64

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,8 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, "Message from proc #1");
+  //context_kload(&pcb[0], hello_fun, "Message from proc #1");
+  context_kload(&pcb[0], hello_fun, "");
   //context_uload(&pcb[0], "/bin/hello"); 
   context_uload(&pcb[1], "/bin/pal", NULL, NULL); 
   //context_kload(&pcb[1], hello_fun, "proc1");
