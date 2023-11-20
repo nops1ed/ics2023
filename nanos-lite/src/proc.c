bfbede28b595941a8569eff44d907e1f955e4b79
tracer-ics2023 <tracer@njuics.org>
2023-11-20 10:41:20 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:41:20 up 41 min,  1 user,  load average: 1.26, 0.60, 0.41

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,14 +28,14 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, "proc0");
+  //context_kload(&pcb[0], hello_fun, "proc0");
   //context_uload(&pcb[1], "/bin/pal"); 
   //context_kload(&pcb[1], hello_fun, "proc1");
   switch_boot_pcb();
 
   Log("Initializing processes...");
   // load program here
-  //naive_uload(NULL, "/bin/nterm");
+  naive_uload(NULL, "/bin/nterm");
 }
 
 Context* schedule(Context *prev) {
