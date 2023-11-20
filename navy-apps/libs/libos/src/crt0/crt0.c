a6a8a79a3b33275b4011ff14eecad10c13743596
tracer-ics2023 <tracer@njuics.org>
2023-11-20 15:06:01 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:06:01 up  5:06,  1 user,  load average: 1.10, 0.78, 0.62

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,50 +1,12 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
-/*
-  |               |
-  +---------------+ <---- ustack.end
-  |  Unspecified  |
-  +---------------+
-  |               | <----------+
-  |    string     | <--------+ |
-  |     area      | <------+ | |
-  |               | <----+ | | |
-  |               | <--+ | | | |
-  +---------------+    | | | | |
-  |  Unspecified  |    | | | | |
-  +---------------+    | | | | |
-  |     NULL      |    | | | | |
-  +---------------+    | | | | |
-  |    ......     |    | | | | |
-  +---------------+    | | | | |
-  |    envp[1]    | ---+ | | | |
-  +---------------+      | | | |
-  |    envp[0]    | -----+ | | |
-  +---------------+        | | |
-  |     NULL      |        | | |
-  +---------------+        | | |
-  | argv[argc-1]  | -------+ | |
-  +---------------+          | |
-  |    ......     |          | |
-  +---------------+          | |
-  |    argv[1]    | ---------+ |
-  +---------------+            |
-  |    argv[0]    | -----------+
-  +---------------+
-  |      argc     |
-  +---------------+ <---- cp->GPRx
-  |               |
-*/
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  int argc = *(int *)args; 
-  char **argv = (char **)args + 1; 
-  char **envp = argv + argc;
-  while(envp != NULL) envp++;
-  environ = envp;
-  exit(main(argc, argv, envp));
+  char *empty[] =  {NULL };
+  environ = empty;
+  exit(main(0, empty, empty));
   assert(0);
 }
