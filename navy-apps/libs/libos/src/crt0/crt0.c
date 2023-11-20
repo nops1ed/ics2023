c49fd36c45dfa0b333ff7c4ab73c726e6a8d235f
tracer-ics2023 <tracer@njuics.org>
2023-11-20 16:12:49 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:12:49 up  6:13,  1 user,  load average: 0.76, 0.56, 0.62

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -40,6 +40,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
+  /*
   int argc = *((int *)args); 
   printf("So the argc is %d\n",argc);
   char **argv = (char **)args + 1; 
@@ -48,6 +49,35 @@ void call_main(uintptr_t *args) {
   char **envp = argv + argc;
   while(*envp != NULL) envp++;
   environ = envp;
+  exit(main(argc, argv, envp));
+  assert(0);
+  */
+
+ // printf("arg is %p\n", args);
+  // write(1, "here\n", 10);
+  int argc = *((int *)args);
+
+  char **pargs = (char **)args + 1;
+  char **argv = pargs;
+  // printf("\n\n\n\n");
+  // printf("argv0 is  %s\n", argv[0]);
+  // if (strcmp(argv[0], "/bin/hello"))
+  //   exit(1);
+  // printf("argv1 is at %s\n", argv[1]);
+
+  while (*pargs != NULL)
+    pargs++;
+
+  // printf("arg is %p\n", args);
+  pargs += 1;
+  char **envp = (char **)pargs;
+  // printf("arg is %s\n", argv[0]);
+  // char *empty[] = {NULL};
+  environ = envp;
+  // if (environ != NULL)
+  //   printf("env0 is at %s\n", environ[0]);
+  // printf("env1 is at %s\n", environ[1]);
+  // printf("arg is %p\n", args);
   exit(main(argc, argv, envp));
   assert(0);
 
