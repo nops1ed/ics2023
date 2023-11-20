3389faa2e6e4f72e2848b0a1b40ff994f5e20edc
tracer-ics2023 <tracer@njuics.org>
2023-11-20 14:56:32 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:56:32 up  4:57,  1 user,  load average: 1.25, 0.82, 0.57

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -40,6 +40,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
+  printf("Running here\n");
   int argc = *(int *)args; 
   char **argv = (char **)args + 1; 
   char **envp = argv + argc;
