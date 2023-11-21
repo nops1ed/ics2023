f76eb9ad3a7eb5458b86828966a9f178e7ad389a
tracer-ics2023 <tracer@njuics.org>
2023-11-21 15:44:15 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:44:15 up  2:00,  1 user,  load average: 0.00, 0.12, 0.21

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -151,10 +151,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   if (argv){
     for (; argv[argc]; ++argc){}
   }
-  //char **args = (char **)malloc(sizeof(char*) * argc);
-  //char **envs = (char **)malloc(sizeof(char*) * envc);
-  char *args[argc];
-  char *envs[envc];
+  char **args = (char **)malloc(sizeof(char*) * argc);
+  char **envs = (char **)malloc(sizeof(char*) * envc);
   
   printf("it is safe now \n");
   /* Copy String Area. */
