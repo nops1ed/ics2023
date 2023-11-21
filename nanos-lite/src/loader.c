cc70bcf49df91d180320a8055969c07129f3f315
tracer-ics2023 <tracer@njuics.org>
2023-11-21 15:10:38 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:10:38 up  1:26,  1 user,  load average: 0.38, 0.24, 0.16

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -150,6 +150,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   Area stack = RANGE((char *)(uintptr_t)pcb, (char *)(uintptr_t)pcb + STACK_SIZE);
   /* deploy user stack layout. */
   char *brk = stack.end;
+  printf("So the brk now is %p\n", brk);
   int argc = _len_of_array(argv);
   int envc = _len_of_array(envp);
   char **args = (char **)malloc(sizeof(char*) * argc);
