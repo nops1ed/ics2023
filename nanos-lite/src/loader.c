b87100534ebfa217c88e9ff8ffe454434a8b689f
tracer-ics2023 <tracer@njuics.org>
2023-11-21 15:39:38 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:39:38 up  1:55,  1 user,  load average: 0.10, 0.25, 0.27

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -174,12 +174,15 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   }
 
   intptr_t *ptr_brk = (intptr_t *)brk;
-  //*(--ptr_brk) = 0;
+  *(--ptr_brk) = 0;
   ptr_brk -= envc;
   for (int i = 0; i < envc; ++i)  ptr_brk[i] = (intptr_t)(envs[i]);
-  //*(--ptr_brk) = 0;
+  *(--ptr_brk) = 0;
   ptr_brk = ptr_brk - argc;
-  for (int i = 0; i < argc; ++i)  ptr_brk[i] = (intptr_t)(args[i]);
+  for (int i = 0; i < argc; ++i)  {
+    printf("So i write %p to %p\n", args[i], ptr_brk[i]);
+    ptr_brk[i] = (intptr_t)(args[i]);
+  }
   *(--ptr_brk) = argc;
 
   Context *ucxt = ucontext(NULL, stack, (void *)entry);
