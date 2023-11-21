89ccde4f9a41b4f6202f308b42b3643884ddce53
tracer-ics2023 <tracer@njuics.org>
2023-11-21 15:05:51 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:05:51 up  1:21,  1 user,  load average: 0.06, 0.10, 0.12

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -139,59 +139,46 @@ Context *context_kload(PCB* pcb, void(*func)(void *), void *args) {
 */
 
 /* Following function just count the number of argv/envp, which is end by "NULL". */
-/*
 static int _len_of_array(char *const argv[]) {
   int count = 0;
   for(; argv[count] != NULL; count++) ;
   return count; 
 }
-*/
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]) {
   uintptr_t entry = loader(pcb, filename);
   Area stack = RANGE((char *)(uintptr_t)pcb, (char *)(uintptr_t)pcb + STACK_SIZE);
   /* deploy user stack layout. */
-  /*
-  char *sp = stack.end;
+  char *brk = stack.end;
   int argc = _len_of_array(argv);
-  char *(args[argc]);
-  for (int i = 0; i < argc; ++i) {
-    sp -= (strlen(argv[i]) + 1);
-    strcpy(sp, argv[i]);
-    args[i] = sp;
-  }
   int envc = _len_of_array(envp);
-  char *(envs[envc]);
-  for (int i = 0; i < envc; ++i)
-  {
-    sp -= (strlen(envp[i]) + 1);
-    strcpy(sp, envp[i]);
-    envs[i] = sp;
+  char **args = (char **)malloc(sizeof(char*) * argc);
+  char **envs = (char **)malloc(sizeof(char*) * envc);
+  
+  /* Copy String Area. */
+  for (int i = 0; i < argc; ++i) {
+    /* Note that it is neccessary to make memory align. */
+    brk -= ROUNDUP(strlen(argv[i]) ,sizeof(int));
+    args[i] = brk;
+    strcpy(brk, argv[i]);
   }
-  char **sp_2 = (char **)sp;
-  --sp_2;
-  *sp_2 = NULL;
-  for (int i = envc - 1; i >= 0; --i)
-  {
-    // printf("loop %d\n", i);
-    --sp_2;
-    *sp_2 = envs[i];
+  for (int i = 0; i < envc; ++i) {
+    brk -= ROUNDUP(strlen(envp[i]), sizeof(int));
+    envs[i] = brk;
+    strcpy(brk, envp[i]);
   }
-  --sp_2;
-  *sp_2 = NULL;
-  for (int i = argc - 1; i >= 0; --i)
-  {
-    --sp_2;
-    // printf("the pos of argv is at stack %p is %p\n", sp_2, args[i]);
-    *sp_2 = args[i];
-    // printf("args is %s\n", args[i]);
-  }
-  --sp_2;
-  *((int *)sp_2) = argc;
-  */
+
+  intptr_t *ptr_brk = (intptr_t *)brk;
+  *(--ptr_brk) = 0;
+  ptr_brk -= envc;
+  for (int i = 0; i < envc; ++i)  ptr_brk[i] = (intptr_t)(envs[i]);
+  *(--ptr_brk) = 0;
+  ptr_brk = ptr_brk - argc;
+  for (int i = 0; i < argc; ++i)  ptr_brk[i] = (intptr_t)(args[i]);
+  *(--ptr_brk) = argc;
 
   Context *ucxt = ucontext(NULL, stack, (void *)entry);
   pcb->cp = ucxt;
-  ucxt->GPRx = (uintptr_t)stack.end;
+  ucxt->GPRx = (intptr_t)ptr_brk;
   //printf("And now in uload stack.end is %p\n", stack.end);
 }
