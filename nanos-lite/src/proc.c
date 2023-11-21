6d945348537035c0c088a36f0af954d598b0e057
tracer-ics2023 <tracer@njuics.org>
2023-11-21 21:03:46 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:03:46 up  7:19,  1 user,  load average: 0.21, 0.09, 0.02

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -9,7 +9,7 @@ Context *context_kload(PCB* pcb, void(*func)(void *), void *args);
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
 
 
-static char *args_pal[] = {"/bin/pallll", "--skip"};
+static char *args_pal[] = {"/bin/pallll", "--skip", "NULL"};
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
@@ -30,10 +30,10 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, "Message from proc #1");
+  //context_kload(&pcb[0], hello_fun, "Message from proc #1");
   //context_uload(&pcb[0], "/bin/hello"); 
   
-  context_uload(&pcb[1], "/bin/pal", args_pal, NULL); 
+  context_uload(&pcb[0], "/bin/pal", args_pal, NULL); 
   //context_kload(&pcb[1], hello_fun, "proc1");
   switch_boot_pcb();
 
