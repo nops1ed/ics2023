89c6796b5ab617220beb968d29c67f296af99527
tracer-ics2023 <tracer@njuics.org>
2023-11-22 09:54:44 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:54:44 up 21 min,  1 user,  load average: 0.05, 0.05, 0.01

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,10 +30,10 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  //context_kload(&pcb[0], hello_fun, "Message from proc #1");
+  context_kload(&pcb[0], hello_fun, "Message from proc #1");
   //context_uload(&pcb[0], "/bin/hello"); 
   
-  context_uload(&pcb[0], "/bin/pal", args_pal, NULL); 
+  context_uload(&pcb[1], "/bin/pal", args_pal, NULL); 
   //context_kload(&pcb[1], hello_fun, "proc1");
   switch_boot_pcb();
 
@@ -43,10 +43,7 @@ void init_proc() {
 }
 
 Context* schedule(Context *prev) {
-  /*
   current->cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
-  */
-  return NULL;
 }
