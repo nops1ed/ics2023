10134b14a2a86cf7bec858ee207b9d60b08f8935
tracer-ics2023 <tracer@njuics.org>
2023-11-22 10:06:16 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:06:16 up 32 min,  1 user,  load average: 0.64, 0.25, 0.14

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -44,8 +44,8 @@ void call_main(uintptr_t *args) {
   printf("In call_main, we got address %p\n", args);
   int argc = *((int *)args); 
   printf("So the argc is %d\n",argc);
-  char **argv = (char **)(args + 1); 
-  printf("CTE0: We gonna track address %p and Argv[0] is %s\n",*argv, *argv[0]);
+  char **argv = (char **)(args + 4); 
+  printf("CTE0: We gonna track address %p and Argv[0] is %s\n",*argv, argv[0]);
   printf("CTE0: We gonna track address %p and Argv[1] is %s\n",*(argv + 1), argv[1]);
   /*
   char **envp = argv + argc;
