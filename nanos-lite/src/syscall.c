baf04384d29cdeb0a7e958ccf69379b9469ff6c8
tracer-ics2023 <tracer@njuics.org>
2023-11-23 15:25:35 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:25:35 up  5:53,  1 user,  load average: 0.95, 0.81, 0.70

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -81,6 +81,7 @@ static void sys_execve(Context *c) {
   fs_curfilename();
   printf("sys_execve(%s, %s, %s)  \n", c->GPR2, c->GPR3, c->GPR4);
 #endif
+printf("Traping sys_execve...\n");
   context_uload(current, (const char *)c->GPR2, (char **const)(uintptr_t)c->GPR3, (char **const)(uintptr_t)c->GPR4);
   switch_boot_pcb();
   yield();
