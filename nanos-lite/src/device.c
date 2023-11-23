7ac091a612e3ba09553d3476fe07d9c2a242c3ee
tracer-ics2023 <tracer@njuics.org>
2023-11-23 14:19:20 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:19:20 up  4:46,  1 user,  load average: 0.08, 0.18, 0.20

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -73,7 +73,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   /* buf does not support lseek. */
   //printf("DEV: read is called and len is %d\n", len);
   //Log("Display info: %d * %d\n", gpuinfo.width, gpuinfo.height);
-  printf("**********\nDisplay info: %d * %d\n**********\n", gpuinfo.width, gpuinfo.height);
+  printf("**********\nDev-Display info: %d * %d\n**********\n", gpuinfo.width, gpuinfo.height);
   char _tmp[32], _tmp2[32];
   sprintf(buf, "WIDTH:%s\nHEIGHT:%s\n", __itoa(gpuinfo.width, _tmp, 10), __itoa(gpuinfo.height, _tmp2, 10));
   return len;
