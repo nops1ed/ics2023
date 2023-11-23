f32a22ae231636b0f765f74020c29535578e9480
tracer-ics2023 <tracer@njuics.org>
2023-11-23 15:06:34 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:06:34 up  5:34,  1 user,  load average: 0.88, 0.68, 0.52

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -41,7 +41,7 @@ static void sh_handle_cmd(const char *cmd) {
     printf("So argv[%d] is %s\n", i, *(argv + i));
   }
   printf("Finished ...\n");
-  execvp(argv[0], argv);
+  execvp(argv[0], argv + 1);
   for(int i = 0; i < argc; i++) 
     free(*(argv + i));
   free(argv);
