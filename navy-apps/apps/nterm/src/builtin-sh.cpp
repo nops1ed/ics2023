947c2af268f4dcb0c3ae295225afc805e5fe09d0
tracer-ics2023 <tracer@njuics.org>
2023-11-23 15:42:57 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:42:57 up  6:10,  1 user,  load average: 0.88, 0.52, 0.54

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -22,12 +22,12 @@ static void sh_prompt() {
   sh_printf("%s:%s$", __USERNAME__, getenv("PATH"));
 }
 
-/*
 static void sh_handle_cmd(const char *cmd) {
   int pos = strcspn(cmd, "\n");
   char *new_str = (char *)malloc(pos + 1); 
   strncpy(new_str, cmd, pos); 
   new_str[pos] = '\0'; 
+  /* Parse arguments list. */ 
   int argc = 1;
   for(int i = 0; *(new_str + i); i++)
     if(*(new_str + i) == ' ') 
@@ -49,73 +49,6 @@ static void sh_handle_cmd(const char *cmd) {
   free(argv);
   free(new_str);
 }
-
-*/
-static size_t get_argc(char *str)
-{
-  size_t i = 0;
-  if (strtok(str, " ") == NULL)
-    return i;
-  else
-    i++;
-
-  while (strtok(NULL, " ") != NULL)
-    i++;
-  return i;
-}
-
-static void get_argv(char *cmd, char **argv)
-{
-  int argc = 0;
-
-  // printf("cmd is %s\n", cmd);
-  argv[argc++] = strtok(cmd, " ");
-  // printf("argv0 %s\n", argv[0]);
-
-  while (true)
-  {
-    argv[argc++] = strtok(NULL, " ");
-    if (argv[argc - 1] == NULL)
-      break;
-    // printf("argv%d %s\n", argc, argv[argc]);
-  }
-  return;
-}
-
-static void sh_handle_cmd(const char *cmd)
-{
-  printf("\n");
-  // printf("here\n");
-  // printf("cmd is %s\n", cmd);
-  if (cmd[0] == '\n')
-    return;
-
-  char cmd_cpy[strlen(cmd) + 1];
-  char *cmd_extract = strtok(strcpy(cmd_cpy, cmd), "\n");
-  char *cmd_name = strtok(cmd_extract, " ");
-  char *args = strtok(NULL, "");
-  // char *cmd_end = cmd_extract + strlen(cmd_extract);
-
-  // char *args = cmd_name + strlen(cmd_name) + 1;
-  // printf("cmd is %s, args is %s\n", cmd_name, args);
-  // if (args > cmd_end)
-  //   args = NULL;
-  cmd_extract = strtok(strcpy(cmd_cpy, cmd), "\n");
-  int argc = get_argc(cmd_extract);
-  // printf("argc is %d\n", argc);
-
-  char *(argv[argc + 1]) = {NULL};
-  cmd_extract = strtok(strcpy(cmd_cpy, cmd), "\n");
-
-  get_argv(cmd_extract, argv);
-  // printf("argv0 is %s\n", argv[0]);
-
-  if (execvp(argv[0], argv) < 0)
-    sh_printf("sh: command not found: %s\n", argv[0]);
-
-  return;
-}
-
 
 void builtin_sh_run() {
   setenv("PATH", "/bin/", 0);
