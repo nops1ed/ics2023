46df5cb1fcea80a5230e0eee1140bea2dc9d2d6b
tracer-ics2023 <tracer@njuics.org>
2023-11-25 09:10:25 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:10:25 up 3 min,  1 user,  load average: 0.36, 0.23, 0.10

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -80,6 +80,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   PTE *page_table_entry = as->ptr + PGD(va) * 8;
   printf("So the satp is %p\n", page_table_entry);
   printf("So the as is %p\n", as->ptr);
+  printf("So the PGD is %d\n", PGD(va) * 8);
   if (!(*page_table_entry & PTE_V)){ 
     void *alloced_page = pgalloc_usr(PGSIZE);
     *page_table_entry = (*page_table_entry & ~PTE_PPN_MASK) | (PTE_PPN_MASK & ((uintptr_t)alloced_page >> 2));
