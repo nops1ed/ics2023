bd350d32cbe1e54a5c23200136f6203d9099d5dd
tracer-ics2023 <tracer@njuics.org>
2023-11-25 15:26:31 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:26:31 up  6:19,  4 users,  load average: 0.27, 0.14, 0.06

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -40,26 +40,4 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   paddr_t paddr = (paddr_t)(pagetable[PX(0, vaddr)]);
   assert(paddr == vaddr);
   return paddr;
-
-/*
-  word_t va_raw = (uint32_t)vaddr;
-  paddr_t *pt_1 = (paddr_t *)guest_to_host((paddr_t)(cpu.csr[REG_SATP]._32 << PG_SHIFT));
-  // printf("pt1[id1(vaddr)] is 0x%x\n", pt_1[PGT1_ID(va_raw)]);
-  assert(pt_1 != NULL);
-  word_t *pt_2 = (word_t *)guest_to_host(pt_1[PGT1_ID(va_raw)]);
-  // if (vaddr == 0x7ffffded)
-
-  assert(pt_2 != NULL);
-  paddr_t paddr = (paddr_t)((pt_2[PGT2_ID(va_raw)] & (~0xfff)) | OFFSET(va_raw));
-  // if (vaddr >= 0x40000000 && vaddr <= 0x8000000)
-  // {
-  // printf("mmu translate vaddr %p\n", (void *)(long)vaddr);
-  // printf("translate ui is 0x%x, pt2 id is %x\n", pt_2[PGT2_ID(va_raw)] >> 12, PGT2_ID(va_raw));
-  // printf("mmu translate to paddr %p\n", (void *)(long)paddr);
-  // } // printf("vaddr is 0x%x, paddr is 0x%x\n", vaddr, paddr);
-  assert(vaddr >= 0x40000000 && vaddr <= 0xa1200000);
-  // assert(paddr == vaddr);
-  // return MEM_RET_FAIL;
-  return paddr;
-  */
 }
