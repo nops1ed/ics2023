9957c5c749f75f28b10c4b4d38fe2040ccf2992e
tracer-ics2023 <tracer@njuics.org>
2023-11-25 16:53:12 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:53:12 up  7:46,  5 users,  load average: 0.31, 0.13, 0.08

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -29,7 +29,7 @@
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   printf("\033[31mTraping into mmu translate\033[0m\n");
-  printf("Now satp has val %ld\n", cpu.csr[CSR_SATP].val);
+  printf("Now satp has val %lx\n", cpu.csr[CSR_SATP].val);
   paddr_t *pagetable = (paddr_t *)guest_to_host((paddr_t)(cpu.csr[CSR_SATP].val << PGSHIFT));
   word_t *pte;
   for(int level = 2; level > 0; level--) {
