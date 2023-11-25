f4b5eaaa8373826307e68937c7439a35cd6ef194
tracer-ics2023 <tracer@njuics.org>
2023-11-25 15:56:08 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:56:08 up  6:49,  5 users,  load average: 0.24, 0.19, 0.12

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -133,10 +133,10 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   free(envs);
 
   uintptr_t entry = loader(pcb, filename);
-  printf("safe here\n");
   Area stack;
   stack.start = &pcb->cp;
   stack.end = &pcb->cp + STACK_SIZE;
+  printf("safe here\n");
   Context *ucxt = ucontext(NULL, stack, (void *)entry);
   pcb->cp = ucxt;
   ucxt->GPRx = (intptr_t)ptr_brk;
