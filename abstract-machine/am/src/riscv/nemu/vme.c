ef664c7f15cfd8997f4e40b4e5193d4c494c11f3
tracer-ics2023 <tracer@njuics.org>
2023-11-25 09:28:30 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:28:30 up 22 min,  1 user,  load average: 0.98, 0.50, 0.25

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -12,12 +12,14 @@ static Area segments[] = {      // Kernel memory mappings
 };
 
 #define USER_SPACE RANGE(0x40000000, 0x80000000)
+/*
 #define PGD(X) ((((uintptr_t)X >> 30) & 0x1FF))   //VPN[2]
 #define PMD(X) ((((uintptr_t)X >> 21) & 0x1FF))   //VPN[1]
 #define PTE(X) ((((uintptr_t)X >> 12) & 0x1FF))   //VPN[0]
 #define VA_OFFSET(X) ((uintptr_t)X & 0xFFF)
 #define PTE_PPN_MASK (0x00FFFFFFFFFFF000)
 #define PTE_PPN(X) (((uintptr_t)X & PTE_PPN_MASK) >> 12)
+*/
 
 static inline void set_satp(void *pdir) {
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
@@ -72,11 +74,11 @@ void __am_switch(Context *c) {
   }
 }
 
+/*
 void map(AddrSpace *as, void *va, void *pa, int prot) {
   va = (void *)(((uintptr_t)va) & (~0xfff));
   pa = (void *)(((uintptr_t)pa) & (~0xfff));
 
-  /* The size of each page table entry is 8B. */
   PTE *page_table_entry = as->ptr + PGD(va) * 8;
 
   printf("So the va is %p\n", va);
@@ -105,6 +107,39 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   *page_table_entry = (PTE_PPN_MASK & ((uintptr_t)pa >> 2)) | (PTE_V | PTE_R | PTE_W | PTE_X) | (prot ? PTE_U : 0);
   printf("It is done\n");
 }
+*/
+
+#define VA_VPN_0(x) (((uintptr_t)x & 0x003FF000u) >> 12)
+#define VA_VPN_1(x) (((uintptr_t)x & 0xFFC00000u) >> 22)
+#define VA_OFFSET(x) ((uintptr_t)x & 0x00000FFFu)
+
+#define PTE_PPN_MASK (0xFFFFFC00u)
+#define PTE_PPN(x) (((uintptr_t)x & PTE_PPN_MASK) >> 10)
+
+void map(AddrSpace *as, void *va, void *pa, int prot) {
+  va = (void *)(((uintptr_t)va) & (~0xfff));
+  pa = (void *)(((uintptr_t)pa) & (~0xfff));
+
+  PTE *page_table_entry = as->ptr + VA_VPN_1(va) * 4;
+  // assert((uintptr_t)as->ptr + VA_VPN_1(va) * 4 == get_satp() + VA_VPN_1(va) * 4);
+
+  if (!(*page_table_entry & PTE_V)){ // 说明二级表未分配
+    void *alloced_page = pgalloc_usr(PGSIZE);
+    *page_table_entry = (*page_table_entry & ~PTE_PPN_MASK) | (PTE_PPN_MASK & ((uintptr_t)alloced_page >> 2));
+    *page_table_entry = (*page_table_entry | PTE_V);
+    // printf("二级表未分配\t二级表项地址:%p\t虚拟地址:%p\n", page_table_entry, va);
+    //assert(((PTE_PPN(*page_table_entry) * 4096 + VA_VPN_0(va) * 4) & ~0xFFFFFF) == ((uintptr_t)alloced_page& ~0xFFFFFF));
+  }
+  // 找到二级表中的表项
+  PTE *leaf_page_table_entry = (PTE *)(PTE_PPN(*page_table_entry) * 4096 + VA_VPN_0(va) * 4);
+  // if ((uintptr_t)va <= 0x40100000){
+  //   printf("设置二级表项\t虚拟地址:%p\t实际地址:%p\t表项:%p\n", va, pa, leaf_page_table_entry);
+  // }
+  // 设置PPN
+  *leaf_page_table_entry = (PTE_PPN_MASK & ((uintptr_t)pa >> 2)) | (PTE_V | PTE_R | PTE_W | PTE_X) | (prot ? PTE_U : 0);
+  //assert(PTE_PPN(*leaf_page_table_entry) * 4096 + VA_OFFSET(va) == (uintptr_t)pa);
+}
+
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   Context *kctx = (Context *)(kstack.end - sizeof(Context)); 
