8da3d9adb3201e3aa795aa35664aba5fb9cbe3c7
tracer-ics2023 <tracer@njuics.org>
2023-11-25 16:24:52 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:24:52 up  7:17,  5 users,  load average: 0.08, 0.08, 0.08

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -101,9 +101,10 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
       *pte = PA2PTE(pagetable) | PTE_V;
       pagetable = (pagetable_t)PTE2PA(*pte);
     }
+    printf("pagetable is %p and *pte is %p\n", pagetable, *pte);
   }
   *pte = PA2PTE(pa) | PTE_V;
-  printf("Mapping %p to %p successfully\n", pa, va);
+  //printf("Mapping %p to %p successfully\n", pa, va);
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
