6213d36737dc3937ef661ffd652ddb32764ebe18
tracer-ics2023 <tracer@njuics.org>
2023-11-25 16:40:55 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:40:55 up  7:34,  5 users,  load average: 0.05, 0.03, 0.04

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -99,7 +99,6 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
       pagetable = (pagetable_t)pgalloc_usr(PGSIZE);
       memset(pagetable, 0, PGSIZE);
       *pte = PA2PTE(pagetable) | PTE_V;
-      pagetable = (pagetable_t)PTE2PA(*pte);
     }
     printf("pagetable is %p and *pte is %p\n", pagetable, *pte);
   }
