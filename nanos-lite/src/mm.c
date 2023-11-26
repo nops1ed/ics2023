df9578844a41cb76908b97c600271f739c8aa260
tracer-ics2023 <tracer@njuics.org>
2023-11-26 10:46:14 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:46:14 up  1:38,  1 user,  load average: 0.94, 0.63, 0.33

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,6 +1,8 @@
 #include <memory.h>
+#include <proc.h>
 
 static void *pf = NULL;
+extern PCB *current;
 
 /* The new_page() function manages the heap with a pf pointer to allocate a contiguous memory area of nr_page * 4KB in size, 
 * and returns the *first* address of the area.
@@ -37,6 +39,29 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
+    // assert(brk >= 0x)
+  // f (brk < (uintptr_t)USR_SPACE.start || (brk > (uintptr_t)USR_SPACE.end - USR_STACK_PG_NUM * PGSIZE))
+  // {
+  //   printf("invalid!\n");
+  //   return 0;i
+  // }
+#define PG_MASK ~0xfff
+  if (current->max_brk == 0)
+  {
+    current->max_brk = (brk & ~PG_MASK) ? ((brk & PG_MASK) + PGSIZE) : brk;
+    printf("first malloc is at %p\n", (void *)current->max_brk);
+    return 0;
+  }
+
+  for (; current->max_brk < brk; current->max_brk += PGSIZE)
+  {
+    // printf("malloc new space for virtual %p, brk is %p\n", (void *)current->max_brk, (void *)brk);
+    // printf("malloc new space %p for virtual %p\n", pg_p, (void *)i);
+    // map(&current->as, (void *)current->max_brk, pg_alloc(PGSIZE), PGSIZE);
+    map(&current->as, (void *)current->max_brk, pg_alloc(PGSIZE), 1);
+  }
+
+  // printf("finished malloc\n");
   return 0;
 }
 
