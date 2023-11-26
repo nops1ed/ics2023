d0781c864bd03e52ebf77b8cef86661816b2ebd5
tracer-ics2023 <tracer@njuics.org>
2023-11-26 15:03:36 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:03:36 up  5:56,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -31,6 +31,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   printf("\033[32mNow satp has val %lx\033[0m\n", cpu.csr[CSR_SATP].val);
   word_t va_raw = (uint64_t)vaddr;
   paddr_t *pt_1 = (paddr_t *)guest_to_host((paddr_t)(cpu.csr[CSR_SATP].val << PGSHIFT));
+  printf("pt_1 address is %p\n", pt_1);
   assert(pt_1 != NULL);
   word_t *pt_2 = (word_t *)guest_to_host(pt_1[PX(2, va_raw)]);
   assert(pt_2 != NULL);
