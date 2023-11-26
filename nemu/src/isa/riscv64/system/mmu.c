f0be28da70e7f178ba26f16736932db0b9d166b2
tracer-ics2023 <tracer@njuics.org>
2023-11-26 16:37:29 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:37:29 up  7:30,  1 user,  load average: 0.12, 0.07, 0.02

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -17,74 +17,43 @@
 #include <memory/vaddr.h>
 #include <memory/paddr.h>
 
-#define PGSHIFT         12
-/* extract the three 9-bit page table indices from a virtual address. */
-#define PXMASK          0x1FF // 9 bits
-#define PXSHIFT(level)  (PGSHIFT+(9*(level)))
-#define PX(level, va) ((((uint64_t) (va)) >> PXSHIFT(level)) & PXMASK)
-/* shift a physical address to the right place for a PTE. */
-#define PA2PTE(pa) ((((uint64_t)pa) >> 12) << 10)
-#define PTE2PA(pte) (((pte) >> 10) << 12)
+
+#define VA_VPN_0(x) (((vaddr_t)x & 0x001FF000u) >> 12)
+#define VA_VPN_1(x) (((vaddr_t)x & 0x1FE00000u) >> 21)
+#define VA_VPN_2(x) (((vaddr_t)x & 0xFFC0000000u) >> 30)
+#define VA_OFFSET(x) ((vaddr_t)x & 0x00000FFFu)
+
+#define PTE_PPN_MASK (0x3FFFFFFFFFFC00u)
+#define PTE_PPN(x) (((vaddr_t)x & PTE_PPN_MASK) >> 10)
+
+#define PTE_V 0x01
+#define PTE_A 0x40
+#define PTE_D 0x80
+
+typedef uint64_t PTE;
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  printf("\033[31mTraping into mmu translate\033[0m\n");
-  printf("\033[32mNow satp has val %lx\033[0m\n", cpu.csr[CSR_SATP].val);
-  word_t va_raw = (uint64_t)vaddr;
-  printf("vaddr is %lx\n", vaddr);
-  printf("After shift satp is %lx\n", cpu.csr[CSR_SATP].val << PGSHIFT);
-  paddr_t *pt_1 = (paddr_t *)guest_to_host((paddr_t)(cpu.csr[CSR_SATP].val << PGSHIFT));
-  assert(pt_1 != NULL);
-  word_t *pt_2 = (word_t *)guest_to_host(pt_1[PX(2, va_raw)]);
-  assert(pt_2 != NULL);
-  printf("pt_2 address is %p\n", pt_2);
-  word_t *pt_3 = (word_t *)guest_to_host(pt_2[PX(1, va_raw)]);
-  printf("pt_3 address is %p\n", pt_3);
-  assert(pt_3 != NULL);
+  paddr_t page_table_entry_addr = (cpu.csr[CSR_SATP].val << 12) + VA_VPN_2(vaddr) * 8;
+  PTE page_table_entry = paddr_read(page_table_entry_addr, 8);
+  // if ((page_table_entry & PTE_V) == 0){
+  //   Log("page_table_entry not valid, vaddr: %#x", vaddr);
+  // }
+  paddr_t page1_table_entry_addr = PTE_PPN(page_table_entry) * 4096 + VA_VPN_1(vaddr) * 8;
+  PTE page1_table_entry = paddr_read(page1_table_entry_addr, 8);
+  
+  paddr_t leaf_page_table_entry_addr = PTE_PPN(page1_table_entry) * 4096 + VA_VPN_0(vaddr) * 8;
+  PTE leaf_page_table_entry = paddr_read(leaf_page_table_entry_addr, 8);
+  // if ((leaf_page_table_entry & PTE_V) == 0){
+  //   Log("leaf_page_table_entry not valid, vaddr: %#x, pte_entry: %#x", vaddr, leaf_page_table_entry_addr);
+  // }
 
-/*
-  paddr_t pagetable = (paddr_t)(cpu.csr[CSR_SATP].val << PGSHIFT);
-  paddr_t *pte;
-  for(int level = 2; level > 0; level--) {
-    pte = &pagetable[PX(level, vaddr)];
-    pagetable =PTE2PA(guest_to_host(pte));
-    *pte = PA2PTE(pagetable);
+  if (type == 0){//读
+    paddr_write(leaf_page_table_entry_addr, 8, leaf_page_table_entry | PTE_A);
+  }else if (type == 1){//写
+    paddr_write(leaf_page_table_entry_addr, 8, leaf_page_table_entry | PTE_D);
   }
-  pte = &pagetable[PX(0, vaddr)];
-  paddr_t paddr =  pte | (va_raw & 0xfff);
+  paddr_t pa = PTE_PPN(leaf_page_table_entry) * 4096 + VA_OFFSET(vaddr);
+  // assert(pa == vaddr);
 
-  paddr_t paddr = (paddr_t)((pt_3[PX(0, va_raw)] & (~0xfff)) | (va_raw & 0xfff));
-  assert(vaddr >= 0x40000000 && vaddr <= 0xa1200000);
-  printf("Successfully convert %lx to %x\n", vaddr, paddr);
-  return paddr;
-  */
-  paddr_t paddr = (paddr_t)((pt_3[PX(0, va_raw)] & (~0xfff)) | (va_raw & 0xfff));
-  return paddr;
-
-/*
-  word_t va_raw = (uint64_t)vaddr;
-  paddr_t *pt_1 = (paddr_t *)guest_to_host((paddr_t)(cpu.csr[CSR_SATP].val << PGSHIFT));
-  // printf("pt1[id1(vaddr)] is 0x%x\n", pt_1[PGT1_ID(va_raw)]);
-  assert(pt_1 != NULL);
-  printf("SATP is %x and pt_1 is %p\n", (paddr_t)cpu.csr[CSR_SATP].val, pt_1);
-  printf("Here PX(2, ra_raw) is %ld\n", PX(2, va_raw));
-  word_t *pt_2 = (word_t *)guest_to_host(pt_1[PX(2, va_raw)]);
-  // if (vaddr == 0x7ffffded)
-  printf("Safe here and pt_2 is %p\n",pt_2);
-  assert(pt_2 != NULL);
-  word_t *pt_3 = (word_t *)guest_to_host(pt_2[PX(1, va_raw)]);
-  assert(pt_3 != NULL);
-
-  paddr_t paddr = (paddr_t)((pt_3[PX(0, va_raw)] & (~0xfff)) | (va_raw & 0xfff));
-  // if (vaddr >= 0x40000000 && vaddr <= 0x8000000)
-  // {
-  // printf("mmu translate vaddr %p\n", (void *)(long)vaddr);
-  // printf("translate ui is 0x%x, pt2 id is %x\n", pt_2[PGT2_ID(va_raw)] >> 12, PGT2_ID(va_raw));
-  // printf("mmu translate to paddr %p\n", (void *)(long)paddr);
-  // } // printf("vaddr is 0x%x, paddr is 0x%x\n", vaddr, paddr);
-  assert(vaddr >= 0x40000000 && vaddr <= 0xa1200000);
-  // assert(paddr == vaddr);
-  // return MEM_RET_FAIL;
-  return paddr;
-  */
-
+  return pa;
 }
