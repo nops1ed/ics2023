d61170d11363d31a477ae37204d224f5aa7ff900
tracer-ics2023 <tracer@njuics.org>
2023-11-26 09:42:16 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:42:16 up 35 min,  1 user,  load average: 0.00, 0.05, 0.07

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -32,12 +32,12 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 
   word_t va_raw = (uint64_t)vaddr;
   paddr_t *pt_1 = (paddr_t *)guest_to_host((paddr_t)(cpu.csr[CSR_SATP].val << PGSHIFT));
+  printf("safe here\n");
   assert(pt_1 != NULL);
   word_t *pt_2 = (word_t *)guest_to_host(pt_1[PX(2, va_raw)]);
   assert(pt_2 != NULL);
   word_t *pt_3 = (word_t *)guest_to_host(pt_2[PX(1, va_raw)]);
   assert(pt_3 != NULL);
-  printf("safe here\n");
 
   paddr_t paddr = (paddr_t)((pt_3[PX(0, va_raw)] & (~0xfff)) | (va_raw & 0xfff));
   assert(vaddr >= 0x40000000 && vaddr <= 0xa1200000);
