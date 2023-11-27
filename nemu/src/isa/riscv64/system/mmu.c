ac842a939cbb7771049cbf74402550b2b20da9b0
tracer-ics2023 <tracer@njuics.org>
2023-11-27 15:31:15 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:31:15 up  5:55,  3 users,  load average: 0.26, 0.19, 0.12

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -63,9 +63,9 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   paddr_t page_table_entry_addr = (cpu.csr[CSR_SATP].val << 12);
   //printf("Pagetable is %x\n", page_table_entry_addr);
   PTE page_table_entry = paddr_read(page_table_entry_addr  + PX(2, vaddr) * 8, 8);
-  paddr_t page1_table_entry_addr = PTE2PA(page_table_entry) + PX(1, vaddr) * 8;
+  paddr_t page1_table_entry_addr = PTE2PA(page_table_entry);
 
-  PTE page1_table_entry = paddr_read(page1_table_entry_addr, 8);
+  PTE page1_table_entry = paddr_read(page1_table_entry_addr  + PX(1, vaddr) * 8, 8);
   paddr_t leaf_page_table_entry_addr = PTE2PA(page1_table_entry) + PX(0, vaddr) * 8;
 
   PTE leaf_page_table_entry = paddr_read(leaf_page_table_entry_addr, 8);
