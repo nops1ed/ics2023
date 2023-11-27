ec54478dc903f94ef244d83d1aa646004cd15609
tracer-ics2023 <tracer@njuics.org>
2023-11-27 10:44:25 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:44:25 up  1:08,  1 user,  load average: 0.04, 0.09, 0.13

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -44,6 +44,7 @@ typedef uint64_t PTE;
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   //printf("\033[31mStarting translate\n");
+  /*
   paddr_t pagetable = (cpu.csr[CSR_SATP].val << 12);
   PTE pte; 
   for(int level = 2; level > 0; level--) {
@@ -54,7 +55,8 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   paddr_write(pagetable, 8, pte | MODE_PTE);
   paddr_t pa = PTE2PA(pte) | (vaddr & 0xFFF);
   return pa;
-  /*
+  */
+  paddr_t page_table_entry_addr = (cpu.csr[CSR_SATP].val << 12) + VA_VPN_2(vaddr) * 8;
   PTE page_table_entry = paddr_read(page_table_entry_addr, 8);
 
   paddr_t page1_table_entry_addr = PTE_PPN(page_table_entry) * 4096 + VA_VPN_1(vaddr) * 8;
@@ -69,7 +71,6 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   }
   paddr_t pa = PTE_PPN(leaf_page_table_entry) * 4096 + VA_OFFSET(vaddr);
   //assert(pa == vaddr);
-  */
 
   return pa;
 }
