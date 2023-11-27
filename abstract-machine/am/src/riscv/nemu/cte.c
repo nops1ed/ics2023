3eb1b4074d4236bd196f65cb27f54e65c59655d5
tracer-ics2023 <tracer@njuics.org>
2023-11-27 20:38:24 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:38:24 up 11:02,  3 users,  load average: 0.14, 0.11, 0.08

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,6 @@ void __am_get_cur_as(Context *c);
 void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) {
-  printf("Traping into irq_handle\n");
   __am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
@@ -35,6 +34,7 @@ Context* __am_irq_handle(Context *c) {
       assert(0);
     }
   }
+  printf("Gonna am switch\n");
   __am_switch(c);
   return c;
 }
