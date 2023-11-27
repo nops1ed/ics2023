92c19ba013b4374bef2a31f5b77f92bd1740740d
tracer-ics2023 <tracer@njuics.org>
2023-11-27 22:03:08 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:03:08 up 12:27,  3 users,  load average: 0.18, 0.11, 0.09

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,11 +29,11 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   printf("\033[31mtraping here...\033[0m\n");
-  //context_kload(&pcb[0], hello_fun, "Message from proc #1");
+  context_kload(&pcb[0], hello_fun, "Message from proc #1");
   printf("\033[31mpcb 0 finished ...\033[0m\n");
   //context_uload(&pcb[0], "/bin/hello"); 
   //context_uload(&pcb[1], "/bin/pal", args_pal, NULL); 
-  context_uload(&pcb[0], "/bin/hello", NULL, NULL); 
+  context_uload(&pcb[1], "/bin/pal", NULL, NULL); 
   printf("\033[31mpcb 1 finished ...\033[0m\n");
   //context_kload(&pcb[1], hello_fun, "proc1");
   switch_boot_pcb();
