bfa822aac12a6ce9dcf9c02817f3f8a868fdb578
tracer-ics2023 <tracer@njuics.org>
2023-11-27 17:21:37 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:21:37 up  7:45,  3 users,  load average: 0.87, 0.54, 0.37

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,7 @@ void __am_get_cur_as(Context *c);
 void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) {
-  __am_get_cur_as(c);
+  //__am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
     /* All of the interrupts will be treated as MODE_MACHINE. */
