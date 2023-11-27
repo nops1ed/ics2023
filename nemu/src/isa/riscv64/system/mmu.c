207fb2d661ca6b608197601efa5de0655b54efe1
tracer-ics2023 <tracer@njuics.org>
2023-11-27 15:27:55 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:27:55 up  5:52,  3 users,  load average: 0.02, 0.09, 0.08

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -60,6 +60,29 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   return pa;
 #else
   static int a = 0;
+  paddr_t page_table_entry_addr = (cpu.csr[CSR_SATP].val << 12) + PX(2, vaddr) * 8;
+  //printf("Pagetable is %x\n", page_table_entry_addr);
+  PTE page_table_entry = paddr_read(page_table_entry_addr, 8);
+  paddr_t page1_table_entry_addr = PTE_PPN(page_table_entry) * 4096 + VA_VPN_1(vaddr) * 8;
+
+  PTE page1_table_entry = paddr_read(page1_table_entry_addr, 8);
+  paddr_t leaf_page_table_entry_addr = PTE_PPN(page1_table_entry) * 4096 + VA_VPN_0(vaddr) * 8;
+
+  PTE leaf_page_table_entry = paddr_read(leaf_page_table_entry_addr, 8);
+  if (type == 0){//读
+    paddr_write(leaf_page_table_entry_addr, 8, leaf_page_table_entry | PTE_A);
+  }else if (type == 1){//写
+    paddr_write(leaf_page_table_entry_addr, 8, leaf_page_table_entry | PTE_D);
+  }
+  paddr_t pa = PTE_PPN(leaf_page_table_entry) * 4096 + VA_OFFSET(vaddr);
+  printf("Now pa equals to %lx + %lx = %x\n",PTE_PPN(leaf_page_table_entry) * 4096 , VA_OFFSET(vaddr), pa);
+  a++;
+  //if(a > 200) assert(0);
+  //assert(pa == vaddr);
+
+
+  /*
+   static int a = 0;
   paddr_t page_table_entry_addr = (cpu.csr[CSR_SATP].val << 12) + VA_VPN_2(vaddr) * 8;
   //printf("Pagetable is %x\n", page_table_entry_addr);
   PTE page_table_entry = paddr_read(page_table_entry_addr, 8);
@@ -81,22 +104,6 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   //assert(pa == vaddr);
 
 
-  /*
-  paddr_t page_table_entry_addr = (cpu.csr[CSR_SATP].val << 12) ;
-  PTE page_table_entry = paddr_read(page_table_entry_addr + VA_VPN_2(vaddr) * 8, 8);
-
-  paddr_t page1_table_entry_addr = PTE_PPN(page_table_entry) * 4096;
-  PTE page1_table_entry = paddr_read(page1_table_entry_addr  + VA_VPN_1(vaddr) * 8 , 8);
-  
-  paddr_t leaf_page_table_entry_addr = PTE_PPN(page1_table_entry) * 4096 ;
-  PTE leaf_page_table_entry = paddr_read(leaf_page_table_entry_addr + VA_VPN_0(vaddr) * 8 , 8);
-  if (type == 0){//读
-    paddr_write(leaf_page_table_entry_addr, 8, leaf_page_table_entry | PTE_A);
-  }else if (type == 1){//写
-    paddr_write(leaf_page_table_entry_addr, 8, leaf_page_table_entry | PTE_D);
-  }
-  paddr_t pa = PTE_PPN(leaf_page_table_entry) * 4096 + VA_OFFSET(vaddr);
-  //assert(pa == vaddr);
   */
   return pa;
 #endif
