60622e891fed3498a6c7bba6be88b77b76fd87cf
tracer-ics2023 <tracer@njuics.org>
2023-11-27 14:44:06 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:44:06 up  5:08,  1 user,  load average: 0.48, 0.12, 0.03

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -48,7 +48,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   PTE pte; 
   for(int level = 2; level > 0; level--) {
     pte = paddr_read(pagetable, 8);
-    pagetable = PTE2PA(pte) + PX(level, vaddr) * 8;
+    pagetable = PTE2PA(pte) + PX(level - 1, vaddr) * 8;
   }
   uint64_t MODE_PTE = type == 0 ? PTE_A : PTE_D;
   paddr_write(pagetable, 8, pte | MODE_PTE);
