b6d7029cbd3602de10593254ae6ad4ea23ec6bad
tracer-ics2023 <tracer@njuics.org>
2023-11-27 20:43:29 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:43:29 up 11:07,  3 users,  load average: 0.15, 0.14, 0.10

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -28,6 +28,7 @@ static inline void set_satp(void *pdir) {
   printf("Now pdir is %x\n", pdir);
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
   printf("So the mode is %x\n", mode);
+  printf("and the xlen is %x\n", __riscv_xlen);
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
 }
 
