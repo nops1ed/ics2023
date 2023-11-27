3e0cf3eda39d310667fc505177c62ffae4c104f3
tracer-ics2023 <tracer@njuics.org>
2023-11-27 14:54:10 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:54:10 up  5:18,  1 user,  load average: 0.12, 0.06, 0.01

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -43,12 +43,12 @@ typedef uint64_t PTE;
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   //printf("\033[31mStarting translate\n");
-  paddr_t pagetable = (cpu.csr[CSR_SATP].val << 12) + PX(2, vaddr) * 8;
+  paddr_t pagetable = (cpu.csr[CSR_SATP].val << 12) | (PX(2, vaddr) * 8);
   printf("Pagetable is %x\n", pagetable);
   PTE pte; 
   for(int level = 2; level > 0; level--) {
     pte = paddr_read(pagetable, 8);
-    pagetable = PTE2PA(pte) + PX(level - 1, vaddr) * 8;
+    pagetable = PTE2PA(pte) | (PX(level - 1, vaddr) * 8);
   }
   uint64_t MODE_PTE = type == 0 ? PTE_A : PTE_D;
   paddr_write(pagetable, 8, pte | MODE_PTE);
