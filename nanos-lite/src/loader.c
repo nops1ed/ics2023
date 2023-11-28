8afe4f18a66754ad1522ae1689477d64f6110adf
tracer-ics2023 <tracer@njuics.org>
2023-11-28 15:21:53 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:21:53 up  1:36,  1 user,  load average: 0.04, 0.21, 0.11

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -215,14 +215,12 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   printf("UContext Allocted at %p\n", context);
   printf("Alloced Page Addr: %p\t PTR_BRK_ADDR: %p\n", alloced_page, ptr_brk);
 
-  ptr_brk -= 1;
-  *ptr_brk = 0;//为了t0_buffer
   //设置了sp
   context->gpr[2]  = (uintptr_t)ptr_brk - (uintptr_t)alloced_page + (uintptr_t)as->area.end;
 
   //似乎不需要这个了，但我还不想动
-  context->GPRx = (uintptr_t)ptr_brk - (uintptr_t)alloced_page + (uintptr_t)as->area.end + 4;
-  //context->GPRx = (intptr_t)(ptr_brk);
+  //context->GPRx = (uintptr_t)ptr_brk - (uintptr_t)alloced_page + (uintptr_t)as->area.end + 4;
+  context->GPRx = (intptr_t)(ptr_brk);
 }
 
 
