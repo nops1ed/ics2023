cdd800be03ddb29a2e98721e01ed0c955be8807b
tracer-ics2023 <tracer@njuics.org>
2023-11-29 13:34:43 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:34:43 up  4:06,  1 user,  load average: 0.45, 0.25, 0.22

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,10 +16,11 @@ void* new_page(size_t nr_page) {
 #ifdef HAS_VME
 static void* pg_alloc(int n) {
 /* The parameter of pg_alloc() is the number of bytes to allocate, 
-* but we must ensure that the space requested by AM is always an integer multiple of the page size
+* but we must ensure that the space requested by AM is 
+* always an integer multiple of the page size
 */
   if(n < 0) {
-    printf("\033[31mFatal Error occured: page to be alloced could not be negetive.\033[0m\n");
+    Log("\033[31mFatal Error occured: page to be alloced could not be negative.\033[0m\n");
     assert(0);
   }
   if(n == 0) 
@@ -37,7 +38,7 @@ void free_page(void *p) {
   panic("not implement yet");
 }
 
-/* The brk() system call handler. */
+/* The brk() syscall handler. */
 int mm_brk(uintptr_t brk) {
 #define PG_MASK ~0xfff
   if (current->max_brk == 0) {
