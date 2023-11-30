64d7213390d306804acd90921a317de4780d2d29
tracer-ics2023 <tracer@njuics.org>
2023-11-30 14:58:46 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:58:46 up  4:54,  1 user,  load average: 0.11, 0.15, 0.17

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -35,15 +35,21 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 
   cpu.csr[CSR_MSTATUS].status.MPIE = cpu.csr[CSR_MSTATUS].status.MIE;
   /* Disable Interrupt. */
-  cpu.csr[CSR_MSTATUS].status.MIE = 0;
+  //cpu.csr[CSR_MSTATUS].status.MIE = 0;
   /* Indicate Machine mode. */
-  //cpu.csr[CSR_MSTATUS].status.MPP = 11;
+  //cpu.csr[CSR_MSTATUS].status.MPP = 0xb;
+  if (cpu.csr[CSR_MSTATUS].status.MIE)
+    cpu.csr[CSR_MSTATUS].status.MPIE = 1;
+  else
+    cpu.csr[CSR_MSTATUS].status.MPIE = 0;
+  cpu.csr[CSR_MSTATUS].status.MIE = 0;
+
   return cpu.csr[CSR_MTVEC].val;
 }
 
 word_t isa_query_intr() {
   /* Query clock interrupt. */
-  if (cpu.INTR && (cpu.csr[CSR_MSTATUS].status.MIE == 1)) {
+  if (cpu.INTR && (cpu.csr[CSR_MSTATUS].status.MIE)) {
     cpu.INTR = false;
     return IRQ_TIMER;
   }
