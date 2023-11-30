b8981a458091bc274045f5577a522e170340b9c9
tracer-ics2023 <tracer@njuics.org>
2023-11-30 15:02:41 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:02:41 up  4:58,  1 user,  load average: 0.09, 0.11, 0.15

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -16,19 +16,8 @@
 #include <isa.h>
 #include "../local-include/reg.h"
 
-#ifndef __MACRO_IRQ_NUM__
-#define __MACRO_IRQ_NUM__
-#if defined(__ISA_X86__)
-#define IRQ_TIMER 32        
-#elif defined(__riscv)
 #define IRQ_TIMER 0x8000000000000007 
-#elif defined(__ISA_MIPS32__)
-#define IRQ_TIMER 0      
-#elif defined(__ISA_LOONGARCH32R__)
-#endif
-#endif
 
-#define IRQ_TIMER 0x8000000000000007 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.csr[CSR_MEPC].val = epc;
   cpu.csr[CSR_MCAUSE].val = NO;
