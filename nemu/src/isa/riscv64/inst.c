70901accc192fe9426366f59f4b5be804531c46f
tracer-ics2023 <tracer@njuics.org>
2023-11-30 13:54:05 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:54:05 up  3:49,  1 user,  load average: 0.19, 0.17, 0.06

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -259,8 +259,8 @@ static void ecall_ctrl(Decode *s) {
 
 static void mret_ctrl(Decode *s) {
   s->dnpc = CR(CSR_MEPC).val;
-  //CR(CSR_MSTATUS).status.MIE = CR(CSR_MSTATUS).status.MPIE;
-  //CR(CSR_MSTATUS).status.MPIE = 1;
+  CR(CSR_MSTATUS).status.MIE = CR(CSR_MSTATUS).status.MPIE;
+  CR(CSR_MSTATUS).status.MPIE = 1;
 }
 
 int isa_exec_once(Decode *s) {
