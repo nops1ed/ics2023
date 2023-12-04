f537744780c66021150febb80872cb01de965f1e
tracer-ics2023 <tracer@njuics.org>
2023-12-04 14:32:17 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:32:17 up  4:53,  1 user,  load average: 0.36, 0.38, 0.35

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,7 +30,7 @@ void hello_fun(void *arg) {
 void init_proc() {
  
   //context_uload(&pcb[0], "/bin/menu", NULL, NULL); 
-  context_kload(&pcb[0], hello_fun, "meltdown"); 
+  //context_kload(&pcb[0], hello_fun, "meltdown"); 
   context_uload(&pcb[1], "/bin/pal", args_pal, NULL); 
   switch_boot_pcb();
 
@@ -42,6 +42,7 @@ void init_proc() {
 Context* schedule(Context *prev) {
   //printf("\033[33mschedule: Traping here...\033[0m\n");
   current->cp = prev;
+  /*
   time_chip++;
   if(time_chip > 100) {
     current = &pcb[0];
@@ -49,6 +50,8 @@ Context* schedule(Context *prev) {
   }
   else 
     current = &pcb[1];
+  */
+  current = &pcb[1];
     //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   //printf("\033[33mFinished...\033[0m\n");
   return current->cp;
