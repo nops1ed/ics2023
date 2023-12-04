2df16975f22c6d31b33a981eccbecbb8a60d14fc
tracer-ics2023 <tracer@njuics.org>
2023-12-04 13:50:53 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:50:53 up  4:12,  1 user,  load average: 0.29, 0.22, 0.17

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -8,7 +8,7 @@ void naive_uload(PCB *pcb, const char *filename);
 Context *context_kload(PCB* pcb, void(*func)(void *), void *args);
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
 
-//static char *args_pal[] = {"/bin/pal", "--skip", NULL};
+static char *args_pal[] = {"/bin/pal", "--skip", NULL};
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
   int j = 1;
   while (1) {
     //Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
-    //Log("Message from %s", (uintptr_t)arg);
+    Log("Message from %s", (uintptr_t)arg);
     j ++;
     yield();
   }
@@ -30,8 +30,9 @@ void hello_fun(void *arg) {
 
 void init_proc() {
  
-  context_uload(&pcb[0], "/bin/menu", NULL, NULL); 
-  context_uload(&pcb[1], "/bin/bird", NULL, NULL); 
+  //context_uload(&pcb[0], "/bin/menu", NULL, NULL); 
+  context_kload(&pcb[0], hello_fun, "MEssage from #0\n"); 
+  context_uload(&pcb[1], "/bin/pal", args_pal, NULL); 
   switch_boot_pcb();
 
   Log("Initializing processes...");
