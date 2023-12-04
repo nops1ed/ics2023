ce6d543e1a1ac8f47f74e87ece9706766fbba32a
tracer-ics2023 <tracer@njuics.org>
2023-12-04 14:58:13 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:58:13 up  5:19,  1 user,  load average: 0.13, 0.23, 0.26

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,7 +30,7 @@ void hello_fun(void *arg) {
 void init_proc() {
  
   //context_uload(&pcb[0], "/bin/menu", NULL, NULL); 
-  //context_kload(&pcb[0], hello_fun, "meltdown"); 
+  context_kload(&pcb[0], hello_fun, NULL); 
   context_uload(&pcb[1], "/bin/pal", args_pal, NULL); 
   switch_boot_pcb();
 
