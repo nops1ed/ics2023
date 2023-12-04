f9287beb915edb83f3ecf9d1b9dc7bf801a362f0
tracer-ics2023 <tracer@njuics.org>
2023-12-04 15:49:15 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:49:15 up  6:10,  1 user,  load average: 0.39, 0.46, 0.34

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,7 +23,7 @@ void hello_fun(void *arg) {
   int j = 1;
   while (1) {
     //Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
-    Log("HEllo");
+    //Log("HEllo");
     j ++;
     yield();
   }
