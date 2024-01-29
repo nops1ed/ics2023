b0a470ef1657554b6dc07ef4720c8f4520cf22cf
tracer-ics2023 <tracer@njuics.org>
2024-01-29 10:50:20 +0800

    >  compile NEMU 23225330 NopS1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:50:20 up  1:00,  1 user,  load average: 0.26, 0.29, 0.26

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -7,26 +7,28 @@ extern PCB *current;
   #define PXMASK 0xFFF
 #endif
 
-/* The new_page() function manages the heap with a pf pointer to allocate a contiguous memory area of nr_page * 4KB in size, 
+/* The new_page() function manages the heap with a pf pointer to allocate a contiguous memory area of nr_page * 4KB in size,
 * and returns the *first* address of the area.
 */
 void* new_page(size_t nr_page) {
   void *old = pf;
-  pf = (void *)((char *)old + nr_page * PGSIZE);
+  memset(pf, 0, nr_page * PGSIZE);
+  //pf = (void *)((char *)old + nr_page * PGSIZE);
+  pf += nr_page * PGSIZE;
   return old;
 }
 
 #ifdef HAS_VME
 static void* pg_alloc(int n) {
-/* The parameter of pg_alloc() is the number of bytes to allocate, 
-* but we must ensure that the space requested by AM is 
+/* The parameter of pg_alloc() is the number of bytes to allocate,
+* but we must ensure that the space requested by AM is
 * always an integer multiple of the page size
 */
   if(n < 0) {
     Log("\033[31mFatal Error occured: page to be alloced could not be negative.\033[0m\n");
     assert(0);
   }
-  if(n == 0) 
+  if(n == 0)
     Log("\033[33mThe number of pages to be allocated is 0, \
             which may not meet your expectations.  \
             If there are any issues, please review your implementation again\033[0m\n");
@@ -43,14 +45,24 @@ void free_page(void *p) {
 
 /* The brk() syscall handler. */
 int mm_brk(uintptr_t brk) {
-  if (current->max_brk == 0) {
-    current->max_brk = (brk & PXMASK) ? ((brk & ~PXMASK) + PGSIZE) : brk;
-    printf("\033[33mfirst malloc is at %p\033[0m\n", (void *)current->max_brk);
-    return 0;
+// printf("mm_brk brk: 0x%08lx; max_brk 0x%08lx;", brk, current->max_brk);
+  current->max_brk = ROUNDUP(current->max_brk, PGSIZE); // max_brk and brk are open -- [,brk)
+  if (brk > current->max_brk) {
+    int page_count = ROUNDUP(brk - current->max_brk, PGSIZE) >> 12;
+    uintptr_t pages_start = (uintptr_t)new_page(page_count);
+    for (int i = 0; i < page_count; ++ i) {
+      map(&current->as,
+          (void*)(current->max_brk + i * PGSIZE),
+          (void*)(pages_start + i * PGSIZE),
+          MMAP_READ|MMAP_WRITE
+          );
+    }
+    current->max_brk += page_count * PGSIZE;
+    // printf("--brked-- ");
   }
-  for (; current->max_brk < brk; current->max_brk += PGSIZE)
-    map(&current->as, (void *)current->max_brk, pg_alloc(PGSIZE), 1);
+  // printf("max_brk 0x%08lx\n", current->max_brk);
   return 0;
+
 }
 
 void init_mm() {
